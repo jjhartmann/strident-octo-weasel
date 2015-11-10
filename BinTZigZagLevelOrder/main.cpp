3298af8bfe739f6386e4839fabfc41d10fb1d859
jhartman <jhartman@sfu.ca>
2015-11-09 18:43:46 -0800

    Init for BinTree Traversal
    Inital commit for the work-in-progress of a binary tree traversal in
    level order zig-zag pattern

---
+++ BinTZigZagLevelOrder/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+//  Definition for a binary tree node.
+struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ };
+
+
+typedef vector<vector<int>> SolVec;
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+
+    }
+};
+
+
+
+
+int main()
+{
+    cout << "Hello, World!" << endl;
+    return 0;
+}
