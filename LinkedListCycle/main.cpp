b947d3c9d25e4c45b165e8777d3acbb269951daa
jhartman <jhartman@sfu.ca>
2015-11-18 22:30:30 -0800

    Solution to cycle detection
    Using two pointers, one moving twice the speed as the other, we are able to
    detect whether or not a cycle occures with O(1) space and at most O(2n) time.

--- LinkedListCycle/main.cpp
+++ LinkedListCycle/main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 
-Definition for singly-linked list.
+//Definition for singly-linked list.
 struct ListNode {
     int val;
     ListNode *next;
@@ -12,8 +12,40 @@ struct ListNode {
 
 class Solution {
 public:
-    bool hasCycle(ListNode *head) {
+    bool hasCycle(ListNode *head)
+    {
+        if (head == nullptr)
+        {
+            return false;
+        }
 
+        ListNode *runner = head;
+        ListNode *drunner = head->next;
+
+        int count = 0;
+        while (drunner != nullptr)
+        {
+            // If runner and drunner meet,
+            // Then cycle detected.
+            if (runner == drunner)
+            {
+                return true;
+            }
+
+            // Have runner only increment ever other cycle
+            if (count % 2 == 0)
+            {
+                runner = runner->next;
+            }
+
+            // Have double runner increment every cycle.
+            drunner = drunner->next;
+
+            count++;
+        }
+
+        // Return false if nullptr detected
+        return false;
     }
 };
 
