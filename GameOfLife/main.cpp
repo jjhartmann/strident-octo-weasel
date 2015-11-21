d7fd38c1653db82e42845c9868a1cf5c8589d14b
jhartman <jhartman@sfu.ca>
2015-11-20 23:55:48 -0800

    Init problem for the game of life

---
+++ GameOfLife/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void gameOfLife(vector<vector<int>>& board) {
+
+    }
+};
+
+int main()
+{
+    return 0;
+}
