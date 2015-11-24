2452a7962476d1fb67ce04cc8cf8c50098914b62
jhartman <jhartman@sfu.ca>
2015-11-23 23:56:20 -0800

    Add methods for game of life logic

--- GameOfLife/main.cpp
+++ GameOfLife/main.cpp
@@ -6,42 +6,102 @@ using namespace std;
 class Solution {
 public:
     void gameOfLife(vector<vector<int>>& board) {
-        int hieght = board.size();
-        int width = board.back().size();
+        height = board.size();
+        width = board.back().size();
 
+        // If empty, return nothing
+        if (height == 0 || width == 0)
+        {
+            return;
+        }
 
-        vector<int> rowPrev(width, 0);
-        vector<int> rowCurrent = board[0];
-        vector<int> rowNext = board[1];
+        rowP = vector<int>(width, 0);
+        rowC = board[0];
 
         // Process each row
-        for (int i = 0; i < hieght; ++i)
+        for (int i = 1; i < height+1; ++i)
         {
-            rowCurrent = board[i];
+            if (i < height)
+                rowN = board[i];
+            else
+                rowN = vector<int>(width, 0);
 
-            if (i + 1 < hieght)
-                rowNext = board[i+1];
 
             // Process each column
             for (int j = 0; j < width; ++j)
             {
+                // Preform Game of life logic.
+                int neighbours = getLiveNeighbours(j);
+                if (neighbours < 2 || neighbours > 3)
+                {
+                    // Cell dies
+                    board[i][j] = 0;
+                }
+                else if (neighbours == 3)
+                {
+                    // Lives or spawns
+                    board[i][j] = 1;
+                }
 
-
-
-
-
-
+                // Else, it will remain either 0 or 1.
             }
 
             // Set prev rows
-            rowPrev =  board[i];
+            rowP = rowC;
+            rowC = rowN;
         }
 
 
     }
+
+private:
+    // Calculate the live neighbours.
+    int getLiveNeighbours(int start_j)
+    {
+        int total = 0;
+
+        // Get the number of neighbours by counting box region
+        int count = 0;
+        for (int j = start_j - 1; j <  start_j + 2; ++j, ++count)
+        {
+            if (j >= 0 && j < width)
+            {
+                total = total + rowP[j] + rowN[j];
+
+                // check for cell
+//                if (count == 1 && )
+            }
+        }
+
+        return total;
+    }
+
+    /////////////////////////////////////////
+    // Private Vars
+    int height, width;
+    vector<int> rowP, rowC, rowN;
+
 };
 
 int main()
 {
+    Solution sol;
+
+    vector<vector<int>> test1 =
+            {
+                    {0 , 1, 1, 0, 0, 1, 1, 0},
+                    {0 , 0, 1, 0, 0, 1, 1, 0},
+                    {0 , 1, 0, 0, 1, 1, 1, 0},
+                    {0 , 1, 0, 1, 0, 1, 1, 0},
+                    {0 , 1, 0, 0, 0, 1, 1, 0},
+                    {0 , 1, 0, 0, 0, 1, 1, 0},
+            };
+
+
+    // test Sol
+    sol.gameOfLife(test1);
+
+    cout << "FINISH" << endl;
+
     return 0;
 }
