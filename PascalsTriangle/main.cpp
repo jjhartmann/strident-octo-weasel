d93cb45205ac7b5373d685ffc621340bf79afeb8
jhartman <jhartman@sfu.ca>
2015-11-24 12:51:41 -0800

    Solution to Pascals Triangle
    Has O(n2) space and time complexity.
    Runtime beating 15.18% of all submissions at 0ms.

--- PascalsTriangle/main.cpp
+++ PascalsTriangle/main.cpp
@@ -6,12 +6,36 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
+        if (numRows <= 0)
+            return vector<vector<int>>();
 
+        vector<vector<int>> res(numRows);
+
+        // First row
+        res[0] = vector<int>(1, 1);
+        for (int i = 1; i < numRows; ++i)
+        {
+            int next = 1;
+            res[i].push_back(next);
+            for (int j = 1; j < i + 1; ++j)
+            {
+                if (j < i)
+                    next += res[i-1][j];
+
+                res[i].push_back(next);
+
+                if (j-1 < i)
+                    next -= res[i-1][j-1];
+            }
+        }
+
+        return res;
     }
 };
 
 int main()
 {
-    cout << "Hello, World!" << endl;
+    Solution sol;
+    sol.generate(5);
     return 0;
 }
