9f23adeef603a84fb746e755e390a68075457083
jhartman <jhartman@sfu.ca>
2015-12-03 17:40:11 -0800

    Add method to find single number
    Use bit manipulation, to find a single number in a array of duplicate numbers,
    use XOR.

--- SingleNumberIII/main.cpp
+++ SingleNumberIII/main.cpp
@@ -35,13 +35,40 @@ public:
 };
 
 
+class Solution2
+{
+public:
+    int findSingleNumber(vector<int> nums)
+    {
+        // To find a single number in a string of duplicates.
+        // There is only one number that has a single element, all others numbers come in pairs.
+        // Can use XOR to find the single number. Using O(n) time and O(1) space
+
+        int res = 0;
+        for (auto i : nums)
+        {
+            res ^= i;
+        }
+
+
+        return res;
+
+    }
+};
+
+
 int main()
 {
     Solution sol;
 
-
+    // Test Find two single numbers
     vector<int> test = { 1,2,1,2,3,4,5,4,6,7,6,7,9,8,8};
     sol.singleNumber(test);
+
+    // Test find single number
+    Solution2 sol2;
+    vector<int> test2 = {1, 1, 2, 3, 4, 3, 2, 4, 5, 6, 5, 12, 6, 7, 8, 9, 8, 7, 9 };
+    int singleNum = sol2.findSingleNumber(test2);
 
     cout << "FINISH" << endl;
     return 0;
