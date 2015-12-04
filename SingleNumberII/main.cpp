7f38873804a34522f15ad068319a8c25ff9b5b02
jhartman <jhartman@sfu.ca>
2015-12-03 23:59:52 -0800

    INIT: Single Number II Problem

---
+++ SingleNumberII/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+
+    }
+};
+
+int main()
+{
+    Solution sol;
+    vector<int> test = {1,2,3,4,5,1,2,3,12,4,5,1,2,3,4,5,};
+
+    // TEst
+    sol.singleNumber(test);
+    return 0;
+}
