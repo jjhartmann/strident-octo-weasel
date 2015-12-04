8455e66b5c54b694b1f586a1a3fc2de37811db53
jhartman <jhartman@sfu.ca>
2015-12-03 23:54:40 -0800

    add solution using XOR
    Runtime: O(n)
    Space: O(1)
    :

--- SingleNumberI/main.cpp
+++ SingleNumberI/main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+class Solution {
+public:
+    int singleNumber(vector<int>& nums)
+    {
+        // Iterate through nums and use xor to find the single number in a array of duplicates.
+        int res = 0;
+        for (int num : nums)
+        {
+            res ^= num;
+        }
+
+        return res;
+    }
+};
+
 int main()
 {
-    cout << "Hello, World!" << endl;
+    Solution sol;
+
+    vector<int> test = {1,2,3,4,5,6,7,8,9,11,9,8,7,6,5,4,3,2,1};
+    sol.singleNumber(test);
+
     return 0;
 }
