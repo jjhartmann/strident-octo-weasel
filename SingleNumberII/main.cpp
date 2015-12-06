5613e5cc3721ac34ee5c893e8556907ba5232285
jhartman <jhartman@sfu.ca>
2015-12-05 19:12:45 -0800

    Add method to convert between different base
    Add methods to the main algorithm

--- SingleNumberII/main.cpp
+++ SingleNumberII/main.cpp
@@ -8,12 +8,48 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int sum = 0;
-        
+
+        // Iterate through vector over all integers.
+        for (int num : nums)
+        {
+            string base3 = converttoBase3(num);
+
+            // Sum all the numbers of base3 together
+            int j = base3.size();
+            for (int i = 0; i < base3.size(); ++i, --j)
+            {
+                sum += stringToNumber<int>(base3.substr(i, 0)) * 10 * j;
+            }
+        }
+
+        // Take % 3 for every digit in sum.
+        string tmp = numbertoString<int>(sum);
+        string res;
+        for (int i = 0; i < tmp.size(); ++i)
+        {
+            res = numbertoString<int>(stringToNumber<int>(tmp.substr(i, 0)) % 3) + res;
+        }
+
+        // Convert res back to base 10;
+
+
 
 
     }
 
 private:
+    // Convert base 3 to base 10;
+    template <typename T>
+    T convertToBase10(string str)
+    {
+        int res = 0;
+        int j = str.size() - 1;
+        for (int i = 0; i < str.size(); ++i, --j)
+        {
+            res = stringToNumber<int>(str.substr(i, 0)) * 3 ^ j;
+        }
+    }
+
     // Convert int to a base 3 representation.
     string converttoBase3(int x)
     {
