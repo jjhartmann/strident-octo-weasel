7457d70963bb23270565ba6305c683825b36b9a6
jhartman <jhartman@sfu.ca>
2015-12-05 18:47:25 -0800

    Add method of string conversion to number type and vice versa

--- SingleNumberII/main.cpp
+++ SingleNumberII/main.cpp
@@ -1,12 +1,48 @@
 #include <iostream>
 #include <vector>
+#include <sstream>
 
 using namespace std;
 
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+        int sum = 0;
+        
 
+
+    }
+
+private:
+    // Convert int to a base 3 representation.
+    string converttoBase3(int x)
+    {
+        string res;
+        while (x > 0)
+        {
+            res = numbertoString(x % 3) + res;
+            x = x / 3;
+        }
+
+        return res;
+    }
+
+    // Convert a number to string
+    template <typename T>
+    string numbertoString(T num)
+    {
+        ostringstream ss;
+        ss << num;
+        return ss.str();
+    }
+
+    // Convert a string to a number
+    template <typename T>
+    T stringToNumber(string str)
+    {
+        istringstream ss(str);
+        T res;
+        return ss >> res ? res : 0;
     }
 };
 
