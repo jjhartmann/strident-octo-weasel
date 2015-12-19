33b078e3f05363aa1e71a48c69c36c8fd267509c
jhartman <jhartman@sfu.ca>
2015-12-19 15:03:48 -0800

    Add method to replace all white space with %20

--- ReplaceStringWhiteSpace/main.cpp
+++ ReplaceStringWhiteSpace/main.cpp
@@ -2,11 +2,20 @@
 
 using namespace std;
 
+static string WHITESPACE = "%20";
+
 class Solution
 {
+public:
     static void replaceWhiteSpace(string &str)
     {
-
+        for (int i = 0; i < str.length(); ++i)
+        {
+            if (str[i] == ' ')
+            {
+                str.replace(i, 1, WHITESPACE);
+            }
+        }
     }
 };
 
@@ -14,5 +23,8 @@ class Solution
 int main()
 {
     cout << "Replace string white space with %20" << endl;
+
+    string teststr = "This is a test.";
+    Solution::replaceWhiteSpace(teststr);
     return 0;
 }
