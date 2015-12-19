0d45a2162c54fd29538ba517cf72e071aa880a42
jhartman <jhartman@sfu.ca>
2015-12-19 14:47:30 -0800

    IsPermutation: Add method to isPermutationSort
    Checks whether a string is an anagram of the other by sorting the two strings first.

--- CheckPermuationofStrings/main.cpp
+++ CheckPermuationofStrings/main.cpp
@@ -1,14 +1,37 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 class Solution
 {
 public:
+    bool isPermutationSort(string str1, string str2)
+    {
+        if (str1.length() != str2.length()) return false;
+
+        // Sort the strings
+        sortString(str1);
+        sortString(str2);
+
+        // Check to see if strings are equal.
+        for (int i = 0; i < str1.length(); ++i)
+        {
+            if (str1[i] != str2[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     bool isPermutation(string str1, string str2)
     {
-        vector<int> charmap(256);
+        if (str1.length() != str2.length()) return false;
+
+        vector<int> charmap(256); // Assuming that the strings are composed of ASCII
         for (char c : str1)
         {
             ++charmap[(int) c];
@@ -22,6 +45,12 @@ public:
 
         return true;
     }
+
+private:
+    void sortString(string &str)
+    {
+        sort(str.begin(), str.end());
+    }
 };
 
 int main()
@@ -29,7 +58,8 @@ int main()
     cout << "Check to see if two strings are permuations of eachother." << endl;
 
     Solution sol;
-    sol.isPermutation("abbede", "beedba");
+//    sol.isPermutation("abbede", "beedba");
+    sol.isPermutationSort("abbede", "beedba");
 
     return 0;
 }
