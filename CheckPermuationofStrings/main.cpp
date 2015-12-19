918731dee02eb2cf56735179ab4657ca64470174
jhartman <jhartman@sfu.ca>
2015-12-18 21:50:10 -0800

    Add method to solve permutation problem

--- CheckPermuationofStrings/main.cpp
+++ CheckPermuationofStrings/main.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+class Solution
+{
+public:
+    bool isPermutation(string str1, string str2)
+    {
+        vector<int> charmap(256);
+        for (char c : str1)
+        {
+            ++charmap[(int) c];
+        }
+
+        for (char c : str2)
+        {
+            if (!charmap[(int) c]) return false;
+            --charmap[(int) c];
+        }
+
+        return true;
+    }
+};
+
 int main()
 {
-    cout << "Hello, World!" << endl;
+    cout << "Check to see if two strings are permuations of eachother." << endl;
+
+    Solution sol;
+    sol.isPermutation("abbede", "beedba");
+
     return 0;
 }
