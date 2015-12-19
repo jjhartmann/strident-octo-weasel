e24e367c387108f6814ad5181cd6b97e2e766deb
jhartman <jhartman@sfu.ca>
2015-12-18 20:37:08 -0800

    Question in determining if a string is unique or not

---
+++ DetermineUniqueString/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool isUnique(string str)
+    {
+
+    }
+    
+};
+
+int main()
+{
+    cout << "Determine if string has all unique characters" << endl;
+    return 0;
+}
--- WordLadderII/main.cpp
+++ WordLadderII/main.cpp
@@ -9,6 +9,28 @@ public:
     vector<vector<string>> findLadders(string beginWord, string endWord, unordered_set<string> &wordList) {
 
     }
+
+
+private:
+    // Determine if two strings are connected by a char diffence.
+    bool checkForEdge(string str1, string str2)
+    {
+        bool diff = false;
+        for (int i = 0; i < str1.length(); ++i)
+        {
+            if (str1[i] != str2[i])
+            {
+                if (diff)
+                {
+                    return false;
+                }
+
+                diff = true;
+            }
+        }
+
+        return true;
+    }
 };
 
 int main()
