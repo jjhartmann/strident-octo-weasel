02037fbca47726628cc96e933e13d92034ea77a4
jhartman <jhartman@sfu.ca>
2015-12-18 21:01:28 -0800

    add method to compute whether a string is unique

--- DetermineUniqueString/main.cpp
+++ DetermineUniqueString/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -7,13 +8,30 @@ class Solution
 public:
     bool isUnique(string str)
     {
+        vector<bool> charmap(256);
+        for (char c : str)
+        {
+            c = tolower(c);
+            if (charmap[(int) c])
+            {
+                return false;
+            }
 
+            charmap[(int) c] = true;
+        }
+
+        return true;
     }
-    
+
 };
 
 int main()
 {
     cout << "Determine if string has all unique characters" << endl;
+
+    Solution sol;
+//    sol.isUnique("mistake");
+    sol.isUnique("Anebila");
+
     return 0;
 }
