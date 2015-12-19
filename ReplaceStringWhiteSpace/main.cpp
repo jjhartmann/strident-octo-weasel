371d289d977af04557e7dda49a3bbfabbb0d8960
jhartman <jhartman@sfu.ca>
2015-12-19 14:51:11 -0800

    INIT: Replace String whitespace

---
+++ ReplaceStringWhiteSpace/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution
+{
+    static void replaceWhiteSpace(string &str)
+    {
+
+    }
+};
+
+
+int main()
+{
+    cout << "Replace string white space with %20" << endl;
+    return 0;
+}
