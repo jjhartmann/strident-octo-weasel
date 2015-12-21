d68afe3652de833db5e99e8f6c89ae899aa32820
jhartman <jhartman@sfu.ca>
2015-12-21 15:43:30 -0800

    INIT: Check if two strings are rotations of eachother

---
+++ FindStringRotation/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool isRotation(string s1, string s2)
+    {
+        
+    }
+};
+
+int main()
+{
+    cout << "Check if two strings are rotations of eachother." << endl;
+    return 0;
+}
