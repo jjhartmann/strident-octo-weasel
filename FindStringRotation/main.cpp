84731e62077d69acd88fbc34d744b1d5c7dbde32
jhartman <jhartman@sfu.ca>
2015-12-21 16:03:54 -0800

    Add method to solve the string rotation problem
    Reads through both string twice giving about O(4n)

--- FindStringRotation/main.cpp
+++ FindStringRotation/main.cpp
@@ -5,14 +5,43 @@ using namespace std;
 class Solution
 {
 public:
-    bool isRotation(string s1, string s2)
+    static bool isRotation(string s1, string s2)
     {
-        
+        int N = s1.length();
+        if (N != s2.length() || N < 1) return false;
+
+        char c = s1[0];
+        int pos = 0;
+        while ((pos = s2.find(c, pos)) != string::npos)
+        {
+            bool found = true;
+            for (int i = 0; i < N && found; ++i)
+            {
+                if (s1[i] != s2[(i + pos) % N])
+                {
+                    found = false;
+                    pos = pos + i;
+                }
+            }
+
+            if (found)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 };
 
 int main()
 {
     cout << "Check if two strings are rotations of eachother." << endl;
+//    string test1 = "mybucket";
+//    string test2 = "cketmybu";
+    string test1 = "mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmybucket";
+    string test2 = "mmmmmmmmmmmmmmmmmmmmmmmmmmmybucketmmmmmmmmmm";
+    Solution::isRotation(test1, test2);
+
     return 0;
 }
