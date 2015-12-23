5d00878be87411788c575b72e4d37ed43192f0e9
jhartman <jhartman@sfu.ca>
2015-12-22 19:29:01 -0800

    Add class for Nodes in linked list

--- RemoveDuplicatesFromUnsortedLinkedList/main.cpp
+++ RemoveDuplicatesFromUnsortedLinkedList/main.cpp
@@ -2,9 +2,42 @@
 
 using namespace std;
 
+template <class T>
+class Node
+{
+public:
+    //Node
+    Node (T d)
+    {
+        data = d;
+    }
+
+    Node<T>* getNext()
+    {
+        return next;
+    }
+
+    T getData()
+    {
+        return data;
+    }
+
+    void SetNext(Node<T> *n)
+    {
+        next = n;
+    }
+
+private:
+    T data;
+    Node<T> *next;
+};
+
+
+
+
 int main()
 {
     cout << "Remove duplicates from an unsorted linked list." << endl;
-    
+
     return 0;
 }
