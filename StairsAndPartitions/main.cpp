0f43495844ba409f3472d2055b0997bfb4bb3aaa
jhartman <jhartman@sfu.ca>
2015-12-22 19:39:06 -0800

    INTI: Stairs and Partitions

---
+++ StairsAndPartitions/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution
+{
+public:
+    static int calcStairs(int n)
+    {
+
+
+        return n;
+    }
+};
+
+int main()
+{
+    cout << "Stairs and Partitions" << endl;
+    return 0;
+}
