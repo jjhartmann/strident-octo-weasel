bc8fef5b2eb9f4a7b99b76c8b5831b2088468314
jhartman <jhartman@sfu.ca>
2015-12-29 15:06:23 -0800

    Add Solution class
    Method isPalindromeBuffer uses a temporary vector to store the values of the palindrome.
    Returns true if valid.
    TIME: O(n)
    SPACE: O(n)

--- FindPalindromeSinglyLinkedList/main.cpp
+++ FindPalindromeSinglyLinkedList/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <vector>
 
 using namespace std;
 
@@ -37,6 +38,7 @@ public:
         if (next == nullptr)
         {
             next = tmp;
+            return;
         }
 
         Node<T> *n = next;
@@ -53,9 +55,74 @@ private:
     Node<T> *next;
 };
 
+////////////////////////////////////////////////////////////////
+// Solution class
+template <class T>
+class Solution
+{
+public:
+    // Find if palindrome using a buffer (vector) to store values
+     static bool isPalindromBuffer(Node<T> *head)
+    {
+        vector<T> buffer;
+        Node<T> *n = head;
+        while (n != nullptr)
+        {
+            buffer.push_back(n->getData());
+            n = n->getNext();
+        }
 
+        int j = buffer.size() - 1;
+        for (int i = 0; i < buffer.size(); ++i, --j)
+        {
+            if (buffer[i] != buffer[j])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+};
+
+////////////////////////////////////////////////////////////////
+// delete list
+template <typename T>
+void deleteList(Node<T> *head)
+{
+    while (head != nullptr)
+    {
+        Node<T> *n = head->getNext();
+        head->remove(nullptr);
+        head = n;
+    }
+}
+//main
 int main()
 {
     cout << "Find A palindrom in a singly linked list" << endl;
+
+    Node<string> *oddhead = new Node<string>("a");
+    oddhead->appendToEnd("b");
+    oddhead->appendToEnd("c");
+    oddhead->appendToEnd("b");
+    oddhead->appendToEnd("a");
+
+    Node<string> *evenhead = new Node<string>("a");
+    evenhead->appendToEnd("b");
+    evenhead->appendToEnd("a");
+    evenhead->appendToEnd("a");
+    evenhead->appendToEnd("b");
+    evenhead->appendToEnd("a");
+
+    // Check if it is palindrome
+    bool res = Solution<string>::isPalindromBuffer(oddhead);
+
+    // Delete the linked lists
+    deleteList<string>(oddhead);
+    deleteList<string>(evenhead);
+
     return 0;
 }
