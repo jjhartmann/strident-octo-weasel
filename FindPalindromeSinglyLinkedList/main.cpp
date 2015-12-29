be3fe4c3b29b5416ca46f97838c2064c5ef55a9e
jhartman <jhartman@sfu.ca>
2015-12-29 12:53:06 -0800

    Add  class to handle to the linked list structure

--- FindPalindromeSinglyLinkedList/main.cpp
+++ FindPalindromeSinglyLinkedList/main.cpp
@@ -1,6 +1,58 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
+
+template <class T>
+class Node
+{
+public:
+    Node(T d) :
+        data(d),
+        next(nullptr)
+    {}
+
+    // Get/Set
+    T getData() { return data; }
+    void setData(T d) { data = d; }
+    Node<T>* getNext() { return next; }
+    void setNext(Node<T> *n) { next = n; }
+
+    // remove this node
+    void remove(Node<T> *prev)
+    {
+        if (prev != nullptr)
+        {
+            prev->setNext(next);
+        }
+
+        delete this;
+    }
+
+    // Append new node to the end of the list
+    void appendToEnd(T d)
+    {
+        Node<T> *tmp = new Node<T>(d);
+        if (next == nullptr)
+        {
+            next = tmp;
+        }
+
+        Node<T> *n = next;
+        while (n->getNext() != nullptr)
+        {
+            n = n->getNext();
+        }
+
+        n->setNext(tmp);
+    }
+
+private:
+    T data;
+    Node<T> *next;
+};
+
 
 int main()
 {
