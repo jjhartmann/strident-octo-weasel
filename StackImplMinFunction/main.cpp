93267b6b36e57cfb6239d439e284df96bf5e1a6d
jhartman <jhartman@sfu.ca>
2016-01-01 22:39:29 -0800

    Add: class Stack
    Implements a wrapper class around stack.
    Keeps track of the min value and returns it in O(1) time.

--- StackImplMinFunction/main.cpp
+++ StackImplMinFunction/main.cpp
@@ -1,10 +1,64 @@
 #include <iostream>
+#include <stack>
 
 using namespace std;
+
+class Stack
+{
+public:
+    Stack() :
+        mMin(INT32_MAX)
+    {}
+
+    void push(int d)
+    {
+        if (mMin > d)
+        {
+            mMin = d;
+        }
+        mStack.push(make_pair(d, mMin));
+    }
+
+    int pop()
+    {
+        int ret = mStack.top().first;
+        mStack.pop();
+        mMin = mStack.top().second;
+        return ret;
+    }
+
+    int min()
+    {
+        return mMin;
+    }
+
+private:
+    stack< pair<int, int> > mStack;
+    int mMin;
+};
 
 int main()
 {
     cout << "Implement a stack that has a min function." << endl;
-    
+    Stack myStack;
+    myStack.push(4);
+    myStack.push(1);
+    myStack.push(5);
+    myStack.push(0);
+    myStack.push(7);
+    myStack.push(-8);
+    myStack.push(2);
+    myStack.push(-10);
+
+    int res = myStack.pop();
+    res = myStack.pop();
+    res = myStack.pop();
+    res = myStack.pop();
+    res = myStack.pop();
+    res = myStack.pop();
+    res = myStack.pop();
+    res = myStack.pop();
+
+
     return 0;
 }
