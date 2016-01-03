2bffd72ccc57aa03960b8a97a66bfd84e7522408
jhartman <jhartman@sfu.ca>
2016-01-02 23:11:45 -0800

    INIT: Implement a stack that acts like a stack of plates, ie if a set limit is exceeded, all supsequent data is added to a new internal buffer.

---
+++ LiteralStackofPlates/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Implment a stack where once an internal limit is exceeed, the stack creates a "
+         << "new stack to hold all subsequent data. (literal stack of plates)" << endl;
+    return 0;
+}
