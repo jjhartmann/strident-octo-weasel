59603e661f9072427f95e0bf4ddb72894b77895f
jhartman <jhartman@sfu.ca>
2016-01-06 13:13:11 -0800

    Add solution class
    Initial framework for the final solution

--- TowerOfHanoi/main.cpp
+++ TowerOfHanoi/main.cpp
@@ -1,10 +1,57 @@
 #include <iostream>
+#include <stack>
 
 using namespace std;
+
+enum {
+    AB = 0,
+    AC,
+    BA,
+    BC,
+    CA,
+    CB,
+};
+
+class Solution
+{
+public:
+    static void solveHanoi(int n)
+    {
+        stack<int> towerA;
+        stack<int> towerB;
+        stack<int> towerC;
+
+        // Build the tower A.
+        for ( int i = n; i > 0; --i)
+        {
+            towerA.push(i);
+        }
+
+        // First move
+        if (n % 2 == 0)
+        {
+            towerB.push(towerA.top());
+            towerA.pop();
+        }
+        else
+        {
+            towerC.push(towerA.top());
+            towerA.pop();
+        }
+
+        // Solve the tower iteratively
+        while (!towerA.empty() || !towerB.empty())
+        {
+
+
+        }
+
+    }
+};
 
 int main()
 {
     cout << "Solve the tower of Hanoi by using 3 stacks." << endl;
-    
+
     return 0;
 }
