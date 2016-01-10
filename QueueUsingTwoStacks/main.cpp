616896d0ab03f1543e7f311c2e9d2369217f7fde
jhartman <jhartman@sfu.ca>
2016-01-09 22:13:22 -0800

    Add methods push() and front()
    Push()
    TIME: O(2n)

--- QueueUsingTwoStacks/main.cpp
+++ QueueUsingTwoStacks/main.cpp
@@ -10,13 +10,36 @@ public:
     // Add type T to the queue
     void push(T d)
     {
-            
+        // (1) empty the initial stack and place in stack 2
+        while (!s1.empty())
+        {
+            s2.push(s1.top());
+            s1.pop();
+        }
+
+        // Add data to initial stack
+        s1.push(d);
+
+        // (2) Empty stack 2 and place back in stack 1.
+        while (!s2.empty())
+        {
+            s1.push(s2.top());
+            s2.pop();
+        }
     }
 
     // return and remove the front of the queue
     T front()
     {
+        T tmp = s1.top();
+        s1.pop();
+        return tmp;
+    }
 
+    // Checks if the queue is empty
+    bool isEmpty()
+    {
+        return s1.empty();
     }
 
 private:
@@ -28,5 +51,20 @@ private:
 int main()
 {
     cout << "Implement a queue using two stacks." << endl;
+
+    MyQueue<int> q;
+
+    for (int i = 0; i < 15; ++i)
+    {
+        q.push(i);
+    }
+
+    int res = 0;
+    while (!q.isEmpty())
+    {
+        res = q.front();
+    }
+
+
     return 0;
 }
