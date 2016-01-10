93be952da08a0d5400e36e3077787e02c03bcdbf
jhartman <jhartman@sfu.ca>
2016-01-09 22:32:00 -0800

    Add: slight optimization
    Fix the add() and front() operations.
    The elements in stack 2 only shift over when it is necessary.
    Slight improvement in runtime expense.

--- QueueUsingTwoStacks/main.cpp
+++ QueueUsingTwoStacks/main.cpp
@@ -8,43 +8,55 @@ class MyQueue
 {
 public:
     // Add type T to the queue
-    void push(T d)
+    void add(T d)
     {
-        // (1) empty the initial stack and place in stack 2
-        while (!s1.empty())
-        {
-            s2.push(s1.top());
-            s1.pop();
-        }
-
-        // Add data to initial stack
-        s1.push(d);
-
-        // (2) Empty stack 2 and place back in stack 1.
-        while (!s2.empty())
-        {
-            s1.push(s2.top());
-            s2.pop();
-        }
+        // Always push onto s2 which holds the latest element on the top.
+        s2.push(d);
     }
 
     // return and remove the front of the queue
     T front()
     {
+        // Call the shift elements to make sure that
+        shiftElements();
+
         T tmp = s1.top();
         s1.pop();
         return tmp;
     }
 
+    // Return the first element.
+    T peek()
+    {
+        shiftElements();
+        return s1.top();
+    }
+
     // Checks if the queue is empty
     bool isEmpty()
     {
-        return s1.empty();
+        return s1.empty() && s2.empty();
     }
 
 private:
     stack<T> s1;
     stack<T> s2;
+
+    // Shift the elements form s2 to s1.
+    // This is so we do not always shift the elements around and only when we need it.
+    void shiftElements()
+    {
+        // Only shift when s1 is empty.
+        if (s1.empty())
+        {
+            while (!s2.empty())
+            {
+                s1.push(s2.top());
+                s2.pop();
+            }
+        }
+    }
+
 
 };
 
@@ -56,7 +68,7 @@ int main()
 
     for (int i = 0; i < 15; ++i)
     {
-        q.push(i);
+        q.add(i);
     }
 
     int res = 0;
