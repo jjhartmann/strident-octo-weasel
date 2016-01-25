b6cc69f98639e8723e7ca024a543b1261c44ce79
jhartman <jhartman@sfu.ca>
2016-01-25 00:00:45 -0800

    Add: Function to create a random tree. Not Balanced.

--- CheckBalanceBinaryTree/main.cpp
+++ CheckBalanceBinaryTree/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <vector>
 
 using namespace std;
 
@@ -7,10 +10,17 @@ class Node
 {
 public:
     Node(T d) :
-        data(d),
+        mData(d),
         mLeft(nullptr),
-        mRight(nullptr),
+        mRight(nullptr)
     {}
+
+    ~Node()
+    {
+        mLeft = nullptr;
+        mRight = nullptr;
+        mData = 0;
+    }
 
     Node<T>* getRight()
     {
@@ -56,14 +66,72 @@ void DeleteTree(Node<T> *root)
         return;
     }
 
-    DeleteTree(root->getLeft);
-    DeleteTree(root->getRight);
+    DeleteTree(root->getLeft());
+    DeleteTree(root->getRight());
     delete root;
+    root = nullptr;
+}
+
+//////////////////////////////////
+// Create a random tree
+Node<int>* CreateRandTree()
+{
+    srand(time(nullptr));
+    int MAX = 20;
+    Node<int> *root = new Node<int>(rand() % MAX);
+    vector<Node<int>*> leafs;
+    leafs.push_back(root);
+
+    for (int i = 0; i < 20; ++i)
+    {
+        int rVal = rand() % MAX;
+        Node<int> *n = (leafs[rVal % leafs.size()]);
+        bool leftEmpty = (n->getLeft() == nullptr) ? true : false;
+        bool rightEmpty = (n->getRight() == nullptr) ? true : false;
+
+        if (rVal % 2 && leftEmpty)
+        {
+            n->setLeft(new Node<int>(rVal));
+            leftEmpty = false;
+
+            // Push new leaf
+            leafs.push_back(n->getLeft());
+            if (!rightEmpty)
+            {
+                n = n->getRight();
+            }
+
+        }
+        else if(rightEmpty)
+        {
+            n->setRight(new Node<int>(rVal));
+            rightEmpty = false;
+
+            // Push new leaf
+            leafs.push_back(n->getRight());
+            if (!leftEmpty)
+            {
+                n = n->getLeft();
+            }
+        }
+    }
+
+
+    return root;
 }
 
 
+//////////////////////////////////
+// Main
 int main()
 {
     cout << "Check to see if a binary tree is balanced" << endl;
+
+    Node<int> *root = CreateRandTree();
+
+
+
+    DeleteTree<int>(root);
+
     return 0;
 }
