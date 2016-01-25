ce7afee630c79132957289682230151d8c9a73a5
Jeremy J Hartmann <jhartman@sfu.ca>
2016-01-25 12:02:46 -0800

    Update: .gitignore with xcode projects

---
+++ CheckBinaryTreeBalance/CheckBinaryTreeBalance/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CheckBinaryTreeBalance
+//
+//  Created by Jeremy Hartmann on 2016-01-25.
+//  Copyright © 2016 Jeremy Hartmann. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
