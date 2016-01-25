f833ebacb6cf8ff5af29e07501769dcf65c8e6d1
jhartman <jhartman@sfu.ca>
2016-01-24 23:23:23 -0800

    Add: Tree class. Add: DeleteTree Function
    Tree Class
    - Simple Node class to be used in a binary tree data structure

--- CheckBalanceBinaryTree/main.cpp
+++ CheckBalanceBinaryTree/main.cpp
@@ -2,9 +2,64 @@
 
 using namespace std;
 
+template< class T >
+class Node
+{
+public:
+    Node(T d) :
+        data(d),
+        mLeft(nullptr),
+        mRight(nullptr),
+    {}
+
+    Node<T>* getRight()
+    {
+        return mRight;
+    }
+
+    void setRight(Node<T> *n)
+    {
+        mRight = n;
+    }
+
+    Node<T>* getLeft()
+    {
+        return mLeft;
+    }
+
+    void setLeft(Node<T> *n)
+    {
+        mLeft = n;
+    }
+
+    T getData()
+    {
+        return mData;
+    }
+    
 
 
+private:
+    T mData;
+    Node<T> *mLeft;
+    Node<T> *mRight;
+};
 
+
+///////////////////////////////////
+// Delete Entire Tree
+template <typename T>
+void DeleteTree(Node<T> *root)
+{
+    if (!root)
+    {
+        return;
+    }
+
+    DeleteTree(root->getLeft);
+    DeleteTree(root->getRight);
+    delete root;
+}
 
 
 int main()
