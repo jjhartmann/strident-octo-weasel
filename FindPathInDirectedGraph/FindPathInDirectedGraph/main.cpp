274f59c310cf070d300f54dc102c1a7fd2fe0f8b
jhartman <jhartman@sfu.ca>
2016-01-30 00:22:51 -0800

    Add, Node class
    Build basic Node class to structure directed graph

--- FindPathInDirectedGraph/FindPathInDirectedGraph/main.cpp
+++ FindPathInDirectedGraph/FindPathInDirectedGraph/main.cpp
@@ -2,8 +2,93 @@
 // Main
 #include <iostream>
 #include <vector>
-
+#include <unordered_map>
 using namespace std;
+
+
+template <class T>
+class Node
+{
+public:
+    Node(T d) :
+        mData(d),
+        mItr(mEdge.begin())
+    {}
+
+    // Data Get and Set
+    T getData() { return mData;  }
+    void setData(T d) { mData = d; }
+
+    // Add edge
+    void addEdge(Node<T> *edge)
+    {
+        mEdge.insert(edge);
+    }
+
+    // Get Number of edges
+    int getEdgeCount()
+    {
+        return mEdge.size();
+    }
+
+    // Remove edge based on Node<T>
+    void removeEdge(T d)
+    {
+        unordered_map<T, Node<T>*>::iterator itr = mEdge.find(d);
+        if (itr != mEdge.end())
+        {
+            mEdge.erase(itr);
+        }
+    }
+
+    // Remove based on index
+    void removeEdgeAtIndex(int index)
+    {
+        if (index >= mEdge.size()) return;
+        
+        unordered_map<T, Node<T>*>::iterator itr = mEdge.begin() + index;
+        if (itr != mEdge.end())
+        {
+            mEdge.erase(itr);
+        }
+    }
+
+    // Find a edge
+    Node<T>* findEdge(T d)
+    {
+        return mEdge.find(d);
+    }
+
+    // Set Iterator
+    void resetItr()
+    {
+        mItr = mEdge.begin();
+    }
+
+    // Get next object. Return true if valid, else false. 
+    bool next()
+    {
+        mItr++;
+        if (mItr == mEdge.end())
+            return false;
+
+        return true;
+    }
+
+    // get current node
+    Node<T>* getCurrent()
+    {
+        if (mItr == mEdge.end())
+            return nullptr;
+
+        return *mItr;
+    }   
+private:
+    T mData;
+    unordered_map<T, Node<T>*> mEdge;
+    typename unordered_map<T, Node<T>*>::iterator mItr;
+};
+
 
 int main()
 {
