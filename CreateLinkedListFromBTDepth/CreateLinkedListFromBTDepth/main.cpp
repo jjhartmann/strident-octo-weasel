daaf4b59323dc974a46b87e146bf1077676ae996
jhartman <jhartman@sfu.ca>
2016-01-30 17:12:40 -0800

    INIT: Create a number of linked lists based on the depths of a binary tree.

---
+++ CreateLinkedListFromBTDepth/CreateLinkedListFromBTDepth/main.cpp
@@ -0,0 +1,17 @@
+////////////////////////////////////////////////////////////////
+// Main
+
+#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+
+    cout << "Create a vector linked lists for each depth of a binary tree" << endl;
+
+
+    return 0;
+}
