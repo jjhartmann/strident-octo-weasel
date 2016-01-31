6c4e41c1f5f7cee781283154a916cca3059a3001
jhartman <jhartman@sfu.ca>
2016-01-30 20:48:16 -0800

    INIT: Check to see if a binary tree is a valid binary tree.

---
+++ CheckIfABinaryTreeIsValide/CheckIfABinaryTreeIsValide/main.cpp
@@ -0,0 +1,18 @@
+/////////////////////////////////////////////////////////////////
+// Main
+
+
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+
+
+using namespace std;
+
+int main()
+{
+    cout << "Check to see if a binary tree is in fact a binary tree." << endl;
+
+    return 0;
+}
