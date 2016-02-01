5c8681cb2bd40a4c2a4c8ae923781220dff83232
jhartman <jhartman@sfu.ca>
2016-02-01 00:15:58 -0800

    INIT: Get the next node in a BST given some other node (inorder traversal).

---
+++ GetNextNodeInBST/GetNextNodeInBST/main.cpp
@@ -0,0 +1,15 @@
+////////////////////////////////////////////////////////////////
+// Main
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    cout << "An algorithm to determine the next node given a node in a BST (in-order)" << endl;
+
+
+    return 0;
+}
