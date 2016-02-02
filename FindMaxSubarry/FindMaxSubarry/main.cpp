5a56cc3e80870269de7d10968db046979e277903
jhartman <jhartman@sfu.ca>
2016-02-02 11:42:54 -0800

    Add: function to find max subarray.
    Function returns a vector with the begining and end index of the array. The total is appended as the last element of the vector.
    The algorithm performs in:
    SPACE: O(1)
    TIME : O(n)

--- FindMaxSubarry/FindMaxSubarry/main.cpp
+++ FindMaxSubarry/FindMaxSubarry/main.cpp
@@ -2,14 +2,49 @@
 // Main
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
+vector<int> FindMaximumSubarray(vector<int> &A)
+{
+    int max_here = 0;
+    int max_total = 0;
+
+    vector<int> resultA(3, 0);
+    int index = 0;
+    for (int i : A)
+    {
+        if (max_here == 0)
+        {
+            resultA[0] = index;
+        }
+
+        max_here = max(0, max_here + i);
+
+
+        if (max_here > max_total)
+        {
+            resultA[1] = index;
+        }
+
+        max_total = max(max_total, max_here);
+        ++index;
+    }
+
+    resultA[2] = max_total;
+    return resultA;
+
+}
 
 int main()
 {
 
     cout << "Find the maximum subarray in a given array." << endl;
+
+    vector<int> test = { 5,4,-2,1,-8,6,5,-3,5,8,-9,4,2,1 };
+    vector<int> res = FindMaximumSubarray(test);
 
 
     return 0;
