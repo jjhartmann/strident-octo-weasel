47f0baa371770df5ead69421b8b7f418a07ea3f4
jhartman <jhartman@sfu.ca>
2016-02-02 11:23:22 -0800

    INIT: Find the maximum subarray within a given interger array.

---
+++ FindMaxSubarry/FindMaxSubarry/main.cpp
@@ -0,0 +1,16 @@
+///////////////////////////////////////////////////////////////
+// Main
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+
+    cout << "Find the maximum subarray in a given array."
+
+
+    return 0;
+}
