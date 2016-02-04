175f3dd9cc99142e7a80c83631787f67b88e7ff4
jhartman <jhartman@sfu.ca>
2016-02-03 22:47:07 -0800

    Add: Node class for BST structure.

--- GetNextNodeInBST/GetNextNodeInBST/main.cpp
+++ GetNextNodeInBST/GetNextNodeInBST/main.cpp
@@ -5,6 +5,53 @@
 
 using namespace std;
 
+enum BRANCH {
+    LEFT = 0,
+    RIGHT = 1,
+};
+
+template<class T>
+class Node
+{
+public:
+    Node(T d) :
+        mData(d),
+        mLeft(nullptr),
+        mRight(nullptr)
+    {}
+
+    T getData() { return mData; }
+    void setData(T d) { mData = d;  }
+    
+
+    // Retrieve child
+    Node<T>* getChild(BRANCH id)
+    {
+        return (id == LEFT) ? mLeft : mRight;
+    }
+
+    // Set Child
+    void setChild(BRANCH id, Node<T> *n)
+    {
+        if (id == LEFT)
+        {
+            mLeft = n;
+        }
+        else
+        {
+            mRight = n;
+        }
+    }
+
+
+private:
+    T mData;
+    Node<T> *mLeft;
+    Node<T> *mRight;
+
+};
+
+
 
 int main()
 {
