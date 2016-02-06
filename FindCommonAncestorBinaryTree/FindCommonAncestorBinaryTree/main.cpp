00fc5b76f79202d942c6cb9acd637d20924430a3
jhartman <jhartman@sfu.ca>
2016-02-06 13:16:48 -0800

    INIT: Find the common ancestor of two nodes in a binary string.

---
+++ FindCommonAncestorBinaryTree/FindCommonAncestorBinaryTree/main.cpp
@@ -0,0 +1,16 @@
+////////////////////////////////////////////////////////////////////
+// Main - Find common ancestor
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Given two nodes, find the common ancestor in a binary tree" << endl;
+
+
+
+
+
+    return 0;
+}
