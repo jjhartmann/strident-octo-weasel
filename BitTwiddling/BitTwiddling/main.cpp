5c52372fadcd08a48fe5554ba9c60c2bdaf48b0f
jhartman <jhartman@sfu.ca>
2016-02-07 00:04:17 -0800

    Add, function to merge two int32 numbers.

--- BitTwiddling/BitTwiddling/main.cpp
+++ BitTwiddling/BitTwiddling/main.cpp
@@ -6,11 +6,31 @@
 
 using namespace std;
 
+/////////////////////////////////////////////////////////////
+// Given two 32 bit ints, and two value range. Merge the integers. 
+int MergeInt(int N, int M, int i, int j)
+{
+    int len = j - i + 1;
+    int mmask = ~(~0 << len);
+    
+    // Clear all MSBs
+    M = M & mmask;
+
+    // Clear N range
+    N = N & ~(mmask << i);
+
+    // merge values
+    return N | (M << i);
+
+}
+
+
 
 int main()
 {
     cout << "Collection of Bit Twiddling Hacks" << endl;
-
+    
+    int res = MergeInt(23, 11, 2, 6);
 
     return 0;
 }
