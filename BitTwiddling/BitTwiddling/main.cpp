452b887871d420fe31e810b34e5982734da64d80
jhartman <jhartman@sfu.ca>
2016-02-06 23:40:20 -0800

    INIT: BitTwiddling, collection of hacks and methods.

---
+++ BitTwiddling/BitTwiddling/main.cpp
@@ -0,0 +1,16 @@
+///////////////////////////////////////////////////////////////
+// Main - Bit Twiddling
+
+#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+{
+    cout << "Collection of Bit Twiddling Hacks" << endl;
+
+
+    return 0;
+}
