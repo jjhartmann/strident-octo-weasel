8b62a4c7aa4a541f5cf83ba2f9aaa1b04912a8fd
jhartman <jhartman@sfu.ca>
2016-02-08 11:27:23 -0800

    INIT: sieve of Eratosthenes implementation.

---
+++ sieveoferatosthenes/sieveoferatosthenes/main.cpp
@@ -0,0 +1,16 @@
+////////////////////////////////////////////////////////////////
+// Main: Sieve of Eratosthenes
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    cout << "Sieve of Eratosthenes implementation for finding all primes." << endl;
+
+
+
+    return 0;
+}
