6256114d05c7b1b5b090b72565a59a8b06bcf014
jhartman <jhartman@sfu.ca>
2016-02-08 20:39:30 -0800

    INIT: Find the best fit line in a grid of data points.

---
+++ FindBestFitLineInGrid/FindBestFitLineInGrid/main.cpp
@@ -0,0 +1,18 @@
+/////////////////////////////////////////////////////////////////
+// Main - Find best fit line in grid. 
+
+
+#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+
+    cout << "Find Best Fit Line in a Grid of Data Points" << endl;
+
+
+    return 0;
+}
