3b7a49965ea26477b82223a1cd78a949326056e3
jhartman <jhartman@sfu.ca>
2016-02-08 18:51:14 -0800

    INIT: Cut two squares in half using one line.

---
+++ CutTwoSquaresInHalf/CutTwoSquaresInHalf/main.cpp
@@ -0,0 +1,17 @@
+//////////////////////////////////////////////////////////////
+// Cut two squares in Half
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+
+    cout << "Cut Two Squares In Half with One Line" << endl;
+
+
+
+    return 0;
+}
