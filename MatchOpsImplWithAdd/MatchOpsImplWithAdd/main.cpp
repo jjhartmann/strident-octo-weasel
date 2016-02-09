8aa95e1d21e9c383bfeda2e5340d87d548e0201b
jhartman <jhartman@sfu.ca>
2016-02-08 17:38:27 -0800

    INIT: Implement Math operators with addition only

---
+++ MatchOpsImplWithAdd/MatchOpsImplWithAdd/main.cpp
@@ -0,0 +1,15 @@
+///////////////////////////////////////////////////////////////////
+// Main - Implement Math operators (*, /, -) with +
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    cout << "Implement Math operators (*, \\, -) with +" << endl;
+
+
+    return 0;
+}
