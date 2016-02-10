f05d1675e9ec5f5fb353bcf4abfe328611fb6503
jhartman <jhartman@sfu.ca>
2016-02-09 19:34:46 -0800

    INIT: Skipping steps.

---
+++ SkippingSteps/SkippingSteps/main.cpp
@@ -0,0 +1,19 @@
+///////////////////////////////////////////////////////////////
+// Main - Skipping steps
+
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+
+    cout << "Number of ways to climb n steps when skipping 1, 2, or 3 steps at a time" << endl;
+
+
+
+
+    return 0;
+}
