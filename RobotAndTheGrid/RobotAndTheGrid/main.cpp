bc9f89e959ad39de798abfee7e0ac69546c8c0e8
jhartman <jhartman@sfu.ca>
2016-02-09 22:26:55 -0800

    INIT: Robot travels a Grid

---
+++ RobotAndTheGrid/RobotAndTheGrid/main.cpp
@@ -0,0 +1,15 @@
+///////////////////////////////////////////////////////////////
+// Main - Robot travels the grid
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "How many ways can a robot travel on a grid" << endl;
+
+
+
+    return 0;
+}
