b5d8c8e13652369008516a77e38cdf506700deec
jhartman <jhartman@sfu.ca>
2016-02-10 13:08:07 -0800

    INIT: Return all subsets of a set.

---
+++ ReturnAllSubsetsOfASet/ReturnAllSubsetsOfASet/main.cpp
@@ -0,0 +1,20 @@
+////////////////////////////////////////////////////////////////
+// Main - Return all Subsets of a Set. 
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    cout << "Return all subsets of a set" << endl;
+
+
+
+
+
+
+
+    return 0;
+}
