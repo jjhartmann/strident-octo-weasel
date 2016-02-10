50e7621dc57020ecd03dad0cb040d4c49ab9078f
jhartman <jhartman@sfu.ca>
2016-02-10 13:20:54 -0800

    Add: method to fnd all subsets.

--- ReturnAllSubsetsOfASet/ReturnAllSubsetsOfASet/main.cpp
+++ ReturnAllSubsetsOfASet/ReturnAllSubsetsOfASet/main.cpp
@@ -2,6 +2,7 @@
 // Main - Return all Subsets of a Set. 
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -9,7 +10,25 @@ using namespace std;
 class Solution
 {
 public:
+    template<typename T>
+    static  vector< vector<T> >* ReturnAllSubsets(vector<T> &vec)
+    {
+        vector<vector<T>> *MetaArray = new vector<vector<T>>(); // Don't Own
 
+        for (int i = 0; i < vec.size(); ++i)
+        {
+            vector<T> tmp;
+            for (int j = 0; j <= i; ++j)
+            {
+                tmp.pop_back(vec[j]);
+            }
+
+            // push back into meta
+            MetaArray->push_back(tmp);
+        }
+
+        return MetaArray;
+    }
 };
 
 int main()
