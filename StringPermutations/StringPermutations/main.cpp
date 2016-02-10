e8c7382ad560155f3f78f0194fc24be43a055518
jhartman <jhartman@sfu.ca>
2016-02-10 15:39:15 -0800

    INIT: compute all permuations of a string

---
+++ StringPermutations/StringPermutations/main.cpp
@@ -0,0 +1,17 @@
+//////////////////////////////////////////////////////////////
+// Main - Compute all string permutations
+
+#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+    cout << "Compute all permutations of a string" << endl;
+
+
+
+    return 0;
+}
