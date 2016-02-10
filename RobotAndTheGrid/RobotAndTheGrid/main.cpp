25463706ceb20dab99151f9415d6f36d9965c7ba
jhartman <jhartman@sfu.ca>
2016-02-10 00:13:25 -0800

    Add: method to calculate the total number of possible paths in grid.
    Using DP to reduce runtime cost.

--- RobotAndTheGrid/RobotAndTheGrid/main.cpp
+++ RobotAndTheGrid/RobotAndTheGrid/main.cpp
@@ -18,16 +18,27 @@ public:
     template<typename ... T>
     Grid(int x, int y, Point first, T ... rest) :
         mX(x),
-        mY(y)
+        mY(y),
+        DPBuffer(((x  + 1) * (y + 1)) , 0)
     {
+
+        // Add restricted points to vector. 
         mRestircted.push_back(first);
         Grid(mRestircted, rest...);
     }
 
 
+    ////////////////////////////////////
+    // Get the total possible paths Robot can traverse in place. 
+    int getAllPossiblePathsNoRestrict()
+    {
+        return  allPathsHelper(mX, mY, DPBuffer);
+    }
+
 
 private:
     vector<Point> mRestircted;
+    vector<int> DPBuffer;
     int mX;
     int mY;
 
@@ -43,6 +54,37 @@ private:
     {
         return;
     }
+
+    // Get all paths helper
+    int allPathsHelper(int x, int y, vector<int> &DP)
+    {
+        int tmpx = (x < y) ? x : y;
+        int tmpy = (x < y) ? y : x;
+
+        if (x == 1 || y == 1)
+        {
+            return x + y;
+        }
+
+        if (DP[tmpy*mY + tmpx])
+        {
+            return DP[tmpy*mY + tmpx];
+        }
+
+        DP[tmpy*mY + tmpx] = allPathsHelper(x - 1, y, DP) + allPathsHelper(x, y - 1, DP);
+        return  DP[tmpy*mY + tmpx];
+        
+    }
+
+    // Factorial
+    int fact(int n)
+    {
+        if (n <= 1) return 1;
+
+        n *= fact(n - 1);
+        return n;
+    }
+       
 };
 
 
@@ -52,8 +94,8 @@ private:
 int main()
 {
     cout << "How many ways can a robot travel on a grid" << endl;
-    Grid myGrid(10, 10, Point(2, 4), Point(2, 5), Point(3,9));
-
+    Grid myGrid(20, 20, Point(2, 4));
+    int ans = myGrid.getAllPossiblePathsNoRestrict();
 
     return 0;
 }
