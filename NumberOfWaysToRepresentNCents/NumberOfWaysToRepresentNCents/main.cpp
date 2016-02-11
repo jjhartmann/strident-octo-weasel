609b8897949ea669e01e2724b2382241742aa7ea
jhartman <jhartman@sfu.ca>
2016-02-10 21:15:18 -0800

    INIT: Determent the number of ways to represent n cents.

---
+++ NumberOfWaysToRepresentNCents/NumberOfWaysToRepresentNCents/main.cpp
@@ -0,0 +1,19 @@
+/////////////////////////////////////////////////////////////////
+// Main - Find the number of ways to represent n cents using 
+// 25c, 10c, 5c, 1c
+
+#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+    cout << "Determine the number of ways to represent n cents using quarters, nickels, dimes, and cents." << endl;
+
+
+
+
+    return 0;
+}
