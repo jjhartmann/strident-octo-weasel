80172969aee94ee85f036b75df8cb4337b559458
jhartman <jhartman@sfu.ca>
2016-02-10 20:00:14 -0800

    Add: method to fill in area of a contingent area.
    Use recursion to fill area of contingent image with a specfied value.

--- FillPaintObject/FillPaintObject/main.cpp
+++ FillPaintObject/FillPaintObject/main.cpp
@@ -7,6 +7,8 @@
 
 using namespace std;
 
+typedef pair<int, int> Point;
+
 class Image
 {
 public:
@@ -41,14 +43,55 @@ public:
             }
             cout << endl;
         }
+    }
 
 
+    // Change and replace value with another (Fill)
+    void fill(Point p, int value)
+    {
+        int cVal = mGrid[p.second * mXdim + p.first];
+        fill(mGrid, p, value, cVal);
     }
 
 private:
+    // Private Vars
     vector<int> mGrid;
     int mXdim;
     int mYdim;
+
+    // Methods. 
+    void fill(vector<int> &grid, Point p, int nVal, int cVal)
+    {
+        // Replace value;
+        grid[p.second * mXdim + p.first] = nVal;
+
+        // Check and replace adjacent values. 
+        // Check right
+        if (p.first < (mXdim - 1) && cVal == grid[p.second * mXdim + (p.first + 1)])
+        {
+            
+            fill(grid, Point(p.first + 1, p.second), nVal, cVal);
+        }
+
+        // check left
+        if (p.first > 0 && cVal == grid[p.second * mXdim + (p.first - 1)])
+        {
+            fill(grid, Point(p.first - 1, p.second), nVal, cVal);
+        }
+
+        // check up
+        if (p.second < (mYdim - 1) && cVal == grid[(p.second + 1) * mXdim + p.first])
+        {
+            fill(grid, Point(p.first, p.second + 1), nVal, cVal);
+        }
+
+        // check up
+        if (p.second > 0 && cVal == grid[(p.second - 1) * mXdim + p.first])
+        {
+            fill(grid, Point(p.first, p.second - 1), nVal, cVal);
+        }
+    }
+
 };
 
 int main()
@@ -69,6 +112,11 @@ int main()
 
 
     // Print to screen 
+    img.printToScreen();
+
+    // Fill in from (4, 4)
+    img.fill(Point(4, 4), 88);
+
     img.printToScreen();
 
 
