b92d00819eb04732081b05565c0874ceb5472e9e
jhartman <jhartman@sfu.ca>
2016-02-10 19:09:42 -0800

    INIT: Fill surounding area in with the same value (ie Fill Paint).

---
+++ FillPaintObject/FillPaintObject/main.cpp
@@ -0,0 +1,17 @@
+///////////////////////////////////////////////////////////////
+// Main - Fill surrounding area with same value. 
+
+#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+    cout << "Fill all adjacent values with another value in surrounding areas" << endl;
+
+
+
+    return 0;
+}
