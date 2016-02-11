9ace137c78f2ef95b44b4bb58460c099bed5bf87
jhartman <jhartman@sfu.ca>
2016-02-10 21:55:48 -0800

    Add: method to solve and determine the number of ways to represent given change.

--- NumberOfWaysToRepresentNCents/NumberOfWaysToRepresentNCents/main.cpp
+++ NumberOfWaysToRepresentNCents/NumberOfWaysToRepresentNCents/main.cpp
@@ -6,12 +6,40 @@
 
 using namespace std;
 
+int MakeChange(int n, int denomination)
+{
+    int next_denom = 0;
+    switch (denomination)
+    {
+    case 25:
+        next_denom = 10;
+        break;
+    case 10:
+        next_denom = 5;
+        break;
+    case 5:
+        next_denom = 1;
+        break;
+    case 1:
+    default:
+        return 1;
+        break;
+    }
+
+    int nWays = 0;
+    for (int i = 0; i <= n; i += denomination)
+    {
+        nWays += MakeChange(n - i, next_denom);
+    }
+
+    return nWays;
+}
 
 
 int main()
 {
     cout << "Determine the number of ways to represent n cents using quarters, nickels, dimes, and cents." << endl;
-
+    int res = MakeChange(100, 25);
 
 
 
