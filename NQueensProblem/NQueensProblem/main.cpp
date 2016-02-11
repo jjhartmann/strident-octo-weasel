aa719530164a90f884b5f5c735ccbd79bacbe9a2
jhartman <jhartman@sfu.ca>
2016-02-10 21:57:51 -0800

    INIT: the n-queens problem.

---
+++ NQueensProblem/NQueensProblem/main.cpp
@@ -0,0 +1,19 @@
+//////////////////////////////////////////////////////////////
+// Main - N Queens Problem
+
+#include <iostream>
+
+
+using namespace std;
+
+
+
+int main()
+{
+    cout << "The N-Queens Problem" << endl;
+
+
+
+
+    return 0;
+}
