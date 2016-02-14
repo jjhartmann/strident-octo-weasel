0a85cbee73b9d00928d40aa09b2fa428b3c1a366
Jeremy J Hartmann <jhartman@sfu.ca>
2016-02-13 19:43:03 -0800

    INIT: Simple Implementation of Strassens Matrix Multiplication Algorithm.

---
+++ StrassenAlgorithm/StrassenAlgorithm/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  StrassenAlgorithm
+//
+//  Created by Jeremy Hartmann on 2016-02-13.
+//  Copyright © 2016 Jeremy Hartmann. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Simple Implementation of Strassens Algorithm");
+    }
+    return 0;
+}
