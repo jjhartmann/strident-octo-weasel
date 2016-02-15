7eac4f622f7c1a08624dc1cb36e3583257bb0559
Jeremy J Hartmann <jhartman@sfu.ca>
2016-02-14 22:31:33 -0800

    Add: Matrix class
    Builds a random matrix and buffers the boraders with zero to make power of 2.

--- StrassenAlgorithm/StrassenAlgorithm/main.m
+++ StrassenAlgorithm/StrassenAlgorithm/main.m
@@ -8,10 +8,72 @@
 
 #import <Foundation/Foundation.h>
 
+
+////////////////////////////////////////////////////////////////
+/// Matrix Class
+@interface P2Matrix : NSObject
+
+@property NSInteger *mMatrix;
+
+- (id) initWithPower2: (NSInteger) n inMax:(NSInteger) max;
+
+@end
+
+@implementation P2Matrix
+
+- (void) dealloc {
+    if (_mMatrix != nil)
+    {
+        free(_mMatrix);
+        _mMatrix = nil;
+    }
+}
+
+- (id) initWithPower2:(NSInteger)n inMax:(NSInteger) max
+{
+    self = [super init];
+    if (self)
+    {
+        // build random matrix power of 2
+        NSInteger pw2 = ceil(log2(n));
+        NSInteger pwN = pow(2, pw2);
+        BOOL pad = (pwN == n) ? NO : YES;
+        
+        // Create matrix buffer
+        _mMatrix = (NSInteger*) malloc(sizeof(NSInteger) * pwN * pwN);
+        
+        // Build matrix
+        for (NSInteger i = 0; i < pwN; ++i)
+        {
+            for (NSInteger j = 0; j < pwN; ++j)
+            {
+                NSInteger tmp = 0;
+                if (i < n && j < n)
+                {
+                    tmp = random() % max;
+                }
+                
+                _mMatrix[i * pwN + j] = tmp;
+                
+            }
+        }
+        
+        
+    }
+    
+    return self;
+}
+
+@end
+
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Simple Implementation of Strassens Algorithm");
+        NSLog(@"Simple Implementation of Strassen's Algorithm");
+        
+        P2Matrix *mat = [[P2Matrix alloc] initWithPower2:6 inMax:50];
+        
     }
     return 0;
 }
