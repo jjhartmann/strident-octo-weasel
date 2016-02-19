cf03202898581e23ec8d6ed05ac1d7fc6d7c420c
jhartman <jhartman@sfu.ca>
2016-02-18 23:08:51 -0800

    INIT: Stacking stricly larger boxes on top of eachother.

---
+++ StackingBoxes_VS/StackingBoxes_VS/main.cpp
@@ -0,0 +1,19 @@
+//////////////////////////////////////////////////
+// Main  - Stacking Boxes
+
+
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+
+int main()
+{
+    cout << "Stack boxes on top of each other with strictly larger boxes on top." << endl;
+
+
+
+    return 0;
+}
