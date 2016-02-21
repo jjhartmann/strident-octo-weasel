639d4084c12a4933917c8f90b231d71e435cfd2a
jhartman <jhartman@sfu.ca>
2016-02-21 01:09:43 -0800

    Add: CreatRandomBoxes Function
    Creates a set of n random boxes.

--- StackingBoxes_VS/StackingBoxes_VS/main.cpp
+++ StackingBoxes_VS/StackingBoxes_VS/main.cpp
@@ -4,15 +4,48 @@
 
 #include <iostream>
 #include <vector>
+#include <ctime>
+#include <cstdlib>
 
 
 using namespace std;
 
+struct Box
+{
+    Box() :
+        height(0),
+        width(0),
+        depth(0)
+    {}
+
+    Box(int h, int w, int d) :
+        height(h),
+        width(w),
+        depth(d)
+    {}
+
+    int height;
+    int width;
+    int depth;
+};
+
+////////////////////////////////////////////////////////////////
+// Create Random Boxes
+void CreateRandomBoxes(vector<Box> &boxes, int n, int MAX)
+{
+    srand(time(0));
+
+    for (int i = 0; i < n; ++i)
+    {
+        Box b(rand() % MAX, rand() % MAX, rand() % MAX);
+        boxes.push_back(b);
+    }
+}
 
 int main()
 {
     cout << "Stack boxes on top of each other with strictly larger boxes on top." << endl;
-
+    
 
 
     return 0;
