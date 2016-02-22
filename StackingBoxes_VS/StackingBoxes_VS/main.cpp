1af23da424f35a6efdd75093cfc260ff6babdb5b
jhartman <jhartman@sfu.ca>
2016-02-21 18:21:33 -0800

    Add: Merge method
    Merges contents of a box array in sorted order.

--- StackingBoxes_VS/StackingBoxes_VS/main.cpp
+++ StackingBoxes_VS/StackingBoxes_VS/main.cpp
@@ -29,6 +29,13 @@ struct Box
     int depth;
 };
 
+enum BOXDIM
+{
+    HEIGHT = 0,
+    WIDTH,
+    DEPTH
+};
+
 ////////////////////////////////////////////////////////////////
 // Create Random Boxes
 void CreateRandomBoxes(vector<Box> &boxes, int n, int MAX)
@@ -47,27 +54,90 @@ void CreateRandomBoxes(vector<Box> &boxes, int n, int MAX)
 class Solution
 {
 public:
-    static void StackBoxes(vector<Box> &boxes)
+    static vector<Box> StackBoxes(vector<Box> &boxes)
     {
+        // Sort on the w, h, and d. Merge sort. 
 
     }
 
 private:
-    static void StackBoxes(vector<Box> &boxes, int a, int b)
+    static void StackBoxes(vector<Box> &boxes, int a, int b, BOXDIM dim)
     {
 
         if (a - b <= 0) return;
 
         int mid = ceil((a - b) / 2);
-        StackBoxes(boxes, a, mid);
-        StackBoxes(boxes, mid + 1, b);
+        StackBoxes(boxes, a, mid, dim);
+        StackBoxes(boxes, mid + 1, b, dim);
 
         // Merge results
-        merge(boxes, a, mid, mid + 1, b);
+        merge(boxes, a, mid, mid + 1, b, dim);
     }
 
-    static void merge(vector<Box> &boxes, int a, int b, int c, int d)
+    static void merge(vector<Box> &boxes, int a, int b, int c, int d, BOXDIM dim)
     {
+        int tmpmin = a;
+        int tmpmax = d;
+
+        int vala = 0;
+        int valb = 0;
+        vector<Box> buffer;
+
+        while (a <= b && c <= d)
+        {
+            switch (dim)
+            {
+            case 0: // HEIGHT
+                vala = boxes[a].height;
+                valb = boxes[c].height;
+
+                break;
+
+            case 1: // WIDTH
+                vala = boxes[a].width;
+                valb = boxes[c].width;
+                break;
+
+            case 2: // DEPTH
+                vala = boxes[a].depth;
+                valb = boxes[c].depth;
+                break;
+            }
+
+            // Build sorted vec
+            if (vala < valb)
+            {
+                buffer.push_back(boxes[a]);
+                a++;
+            }
+            else
+            {
+                buffer.push_back(boxes[b]);
+                b++;
+            }
+        }
+
+        // Fill rest of buffer. 
+        while (a <= b)
+        {
+            buffer.push_back(boxes[a]);
+            a++;
+        }
+
+        while (c <= d)
+        {
+            buffer.push_back(boxes[b]);
+            b++;
+        }
+
+        // Place Sorted Vec into boxes array.
+        int index = 0;
+        while (tmpmin <= tmpmax)
+        {
+            boxes[tmpmin] = buffer[index];
+            ++tmpmin;
+            ++index;
+        }
 
     }
 
