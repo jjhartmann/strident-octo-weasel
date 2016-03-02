e69f7cf500e2ce35eae31b4ab4bda1b53e4dfd36
Jeremy J Hartmann <jhartman@sfu.ca>
2016-03-02 13:15:01 -0800

    ADD: method declarations.

--- Objc-HEAPSORT-Impl/Objc-HEAPSORT-Impl/main.m
+++ Objc-HEAPSORT-Impl/Objc-HEAPSORT-Impl/main.m
@@ -8,15 +8,40 @@
 
 #import <Foundation/Foundation.h>
 
-@interface HEAPSORT : NSObject
+@interface HEAPSORT : NSObject {
+    
+}
 
+// Public method access
+- (void)heapSort:(NSMutableArray *) inArr;
 
 
 @end
 
+// Private Methods
+@interface HEAPSORT (Helpers)
+- (void)buildMaxHeap:(NSMutableArray *)inArr;
+- (void)heapify:(NSMutableArray *)inArr;
+@end
+
 @implementation HEAPSORT
 
+- (void)heapSort:(NSMutableArray *)inArr
+{
+    
+    
+}
 
+// Private Methods
+- (void)buildMaxHeap:(NSMutableArray *)inArr
+{
+    
+}
+
+- (void)heapify:(NSMutableArray *)inArr
+{
+    
+}
 
 @end
 
