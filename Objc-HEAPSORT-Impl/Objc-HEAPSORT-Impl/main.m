1a6ec6e7a20836d61e30f2be1f506ea23fae13ff
Jeremy J Hartmann <jhartman@sfu.ca>
2016-03-03 13:06:55 -0800

    Add: method to retrive the right and left child in heap

--- Objc-HEAPSORT-Impl/Objc-HEAPSORT-Impl/main.m
+++ Objc-HEAPSORT-Impl/Objc-HEAPSORT-Impl/main.m
@@ -8,39 +8,54 @@
 
 #import <Foundation/Foundation.h>
 
+
+
+
 @interface HEAPSORT : NSObject {
     
 }
 
 // Public method access
-- (void)heapSort:(NSMutableArray *) inArr;
++ (void)heapSort:(NSMutableArray *) inArr;
 
 
 @end
 
 // Private Methods
 @interface HEAPSORT (Helpers)
-- (void)buildMaxHeap:(NSMutableArray *)inArr;
-- (void)heapify:(NSMutableArray *)inArr;
++ (void)buildMaxHeap:(NSMutableArray *)inArr;
++ (void)heapify:(NSMutableArray *)inArr index:(NSInteger)i;
++ (NSInteger)left:(NSInteger)i;
++ (NSInteger)right:(NSInteger)i;
 @end
 
 @implementation HEAPSORT
 
-- (void)heapSort:(NSMutableArray *)inArr
++ (void)heapSort:(NSMutableArray *)inArr
 {
     
     
 }
 
 // Private Methods
-- (void)buildMaxHeap:(NSMutableArray *)inArr
++ (void)buildMaxHeap:(NSMutableArray *)inArr
 {
     
 }
 
-- (void)heapify:(NSMutableArray *)inArr
++ (void)heapify:(NSMutableArray *)inArr index:(NSInteger)i
 {
     
+}
+
++ (NSInteger)left:(NSInteger)i
+{
+    return 2*i + 1;
+}
+
++ (NSInteger)right:(NSInteger)i
+{
+    return 2*i + 2;
 }
 
 @end
