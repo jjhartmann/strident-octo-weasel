fccd77245daa7f901435ab2544c7890f9329974d
Jeremy J Hartmann <jhartman@sfu.ca>
2016-03-19 11:26:11 -0700

    Add: Implementation for changing character spaces to normalized form.

--- CC1-4-Objc/CC1-4-Objc/main.m
+++ CC1-4-Objc/CC1-4-Objc/main.m
@@ -10,13 +10,41 @@
 #import <Foundation/Foundation.h>
 
 @interface NSString (EXString)
-- (void)normailzeSpace;
+- (NSString *)normailzeSpace;
 @end
 
 @implementation NSString (EXString)
-- (void)normailzeSpace
+- (NSString *)normailzeSpace
 {
+    NSInteger spaceCount = 0;
+    for (int i = 0; i < self.length; ++i)
+    {
+        if ([self characterAtIndex:i] == ' ')
+        {
+            spaceCount++;
+        }
+    }
     
+    NSInteger newLen = self.length + (spaceCount *2);
+    char str[newLen];
+    
+    int j = 0;
+    for (int i = 0; i < self.length; ++i)
+    {
+        str[j] = [self characterAtIndex:i];
+        if (str[j] == ' ')
+        {
+            str[j] = '%';
+            str[j + 1] = '2';
+            str[j + 2] = '0';
+            
+            j += 2;
+        }
+        
+        ++j;
+    }
+    
+    return [NSString stringWithUTF8String:str];
     
 }
 @end
@@ -27,6 +55,15 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        
+        // Trivial way to replace spaces.
+        NSString *test = @"This is a test string.";
+        NSString *result = [test stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet alphanumericCharacterSet]];
+        
+        NSString *test2 = @"This is a test to see if white space gets replaced";
+        NSString *res = [test2 normailzeSpace];
+        
+        
     }
     return 0;
 }
