85f84a10443fc6a3b6e3015c3f6565eefd489c2a
Jeremy J Hartmann <jhartman@sfu.ca>
2016-03-18 22:36:54 -0700

    Add: Method to determine if character string is unique.

--- CC1-1/CC1-1/main.m
+++ CC1-1/CC1-1/main.m
@@ -9,8 +9,30 @@
 #import <Foundation/Foundation.h>
 
 @interface Solution : NSObject
-+ (BOOL)isStringUnique1;
++ (BOOL)isStringUnique1:(NSString *)string;
 + (BOOL)isStringUnique2;
+@end
+
+@implementation Solution
+
++ (BOOL)isStringUnique1:(NSString *)string
+{
+    NSInteger map[256];
+    bzero(&map, sizeof(NSInteger)*256);
+    
+    for (int i = 0; i < [string length]; ++i)
+    {
+        NSInteger index = (NSInteger) [string characterAtIndex:i];
+        ++map[index];
+        if (map[index] > 1)
+        {
+            return NO;
+        }
+    }
+    
+    return YES;
+}
+
 @end
 
 
@@ -18,6 +40,10 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        
+        NSString *test = @"THiscanbe l";
+        BOOL res = [Solution isStringUnique1:test];
+        
     }
     return 0;
 }
