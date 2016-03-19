9c5522cf72c890c2a3d95a80f1227204362bb52d
Jeremy J Hartmann <jhartman@sfu.ca>
2016-03-18 21:50:53 -0700

    INIT: Project for CC1-1, unique string

---
+++ CC1-1/CC1-1/main.m
@@ -0,0 +1,23 @@
+//
+//  main.m
+//  CC1-1
+//
+//  Created by Jeremy Hartmann on 2016-03-18.
+//  Copyright © 2016 Jeremy Hartmann. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Solution : NSObject
++ (BOOL)isStringUnique1;
++ (BOOL)isStringUnique2;
+@end
+
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
