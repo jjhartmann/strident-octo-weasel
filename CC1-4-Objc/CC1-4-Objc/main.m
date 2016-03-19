1fbdb24f28c68fb4049f81908a906b73a80436f5
Jeremy J Hartmann <jhartman@sfu.ca>
2016-03-19 00:26:32 -0700

    INIT: Replace all space with %20.

---
+++ CC1-4-Objc/CC1-4-Objc/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CC1-4-Objc
+//
+//  Created by Jeremy Hartmann on 2016-03-19.
+//  Copyright © 2016 Jeremy Hartmann. All rights reserved.
+//
+//  Replace all ' ' with "%20". 
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
