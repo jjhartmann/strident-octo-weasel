99d0127131fd9d2060230bc800c833264be12f25
Jeremy J Hartmann <jhartman@sfu.ca>
2016-03-19 14:25:23 -0700

    Add: skeleton class extension off of NSArray

--- CC1-6-Objc/CC1-6-Objc/main.m
+++ CC1-6-Objc/CC1-6-Objc/main.m
@@ -8,10 +8,37 @@
 
 #import <Foundation/Foundation.h>
 
+@interface NSArray (EXArray)
+- (NSArray *)rotateMatrix
+@end
+
+@implementation NSArray (EXArray)
+
+- (NSArray *)rotateMatrix
+{
+    
+}
+
+@end
+
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        
+        // Create matrix
+        NSArray *mat = [[NSArray alloc] initWithObjects:
+                         @[@1,@2,@3,@4],
+                         @[@5,@6,@7,@8],
+                         @[@9,@10,@11,@12],
+                         @[@13,@14,@15,@16],nil];
+        
+        
+        
+        
+        
+        
     }
     return 0;
 }
