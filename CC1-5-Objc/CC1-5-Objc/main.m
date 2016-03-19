d43325b4513ee63910bcb086bf2e7d28cb293694
Jeremy J Hartmann <jhartman@sfu.ca>
2016-03-19 12:26:31 -0700

    INIT: Compress string

---
+++ CC1-5-Objc/CC1-5-Objc/main.m
@@ -0,0 +1,52 @@
+//
+//  main.m
+//  CC1-5-Objc
+//
+//  Created by Jeremy Hartmann on 2016-03-19.
+//  Copyright © 2016 Jeremy Hartmann. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (EXString)
+- (NSString *)compress;
+@end
+
+@implementation NSString (EXString)
+- (NSString *)compress
+{
+    NSMutableString *tmp;
+    
+    int charCount = 0;
+    char prevC = 0;
+    for (int i = 0; i < self.length; ++i)
+    {
+        char currC = [self characterAtIndex:i];
+        if (currC != prevC)
+        {
+            if (charCount > 0)
+            {
+                [tmp appendFormat:@"%i", charCount];
+            }
+            prevC = currC;
+            [tmp appendFormat:@"%c", currC];
+            charCount = 0;
+        }
+    }
+    
+    return [NSString stringWithString:tmp];
+    
+}
+@end
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+        NSString *test = @"aabbcccccd";
+        NSString *res = [test compress];
+        
+    }
+    return 0;
+}
