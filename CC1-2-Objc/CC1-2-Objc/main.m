2e107f2e34e685f60aaaf5c229a2c9b1bcaef52d
Jeremy J Hartmann <jhartman@sfu.ca>
2016-03-18 23:43:28 -0700

    Add: Method to reverse strings using c string pointers

--- CC1-2-Objc/CC1-2-Objc/main.m
+++ CC1-2-Objc/CC1-2-Objc/main.m
@@ -8,10 +8,38 @@
 
 #import <Foundation/Foundation.h>
 
+void reverse(char *str)
+{
+    char *prt1 = str;
+    char *prt2 = str;
+    while (*prt2 != '\0')
+    {
+        prt2++;
+    }
+    prt2--;
+    
+    while (prt1 != prt2)
+    {
+        char c = *prt1;
+        *prt1 = *prt2;
+        *prt2 = c;
+        
+        prt1++;
+        prt2--;
+    }
+    
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        
+        char str[] = "This is a test string";
+        reverse(str);
+        
+        
+        
     }
     return 0;
 }
