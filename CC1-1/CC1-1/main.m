f0ff605a68738da0d1f45a4630860e95c0a19eed
Jeremy J Hartmann <jhartman@sfu.ca>
2016-03-18 23:23:14 -0700

    Add: identify unique string
    sort array..
    iterate over it and compare adjacent chars.

--- CC1-1/CC1-1/main.m
+++ CC1-1/CC1-1/main.m
@@ -10,7 +10,7 @@
 
 @interface Solution : NSObject
 + (BOOL)isStringUnique1:(NSString *)string;
-+ (BOOL)isStringUnique2;
++ (BOOL)isStringUnique2:(NSString *)string;
 @end
 
 @implementation Solution
@@ -33,6 +33,30 @@
     return YES;
 }
 
++ (BOOL)isStringUnique2:(NSString *)string
+{
+    // Convert into NSArray
+    NSMutableArray *charArray = [NSMutableArray arrayWithCapacity:string.length];
+    for (int i = 0; i < string.length; ++i)
+    {
+        [charArray addObject:[string substringWithRange:NSMakeRange(i, 1)]];
+    }
+    
+    NSArray *sorted = [charArray sortedArrayUsingComparator:^(NSString *a, NSString *b){
+        return [a compare:b];
+    }];
+    
+    for (int i = 1; i < sorted.count; ++i)
+    {
+        if ([[sorted objectAtIndex:i] isEqualToString:[sorted objectAtIndex:i-1]])
+        {
+            return NO;
+        }
+    }
+    
+    return YES;
+}
+
 @end
 
 
@@ -41,8 +65,8 @@ int main(int argc, const char * argv[]) {
         // insert code here...
         NSLog(@"Hello, World!");
         
-        NSString *test = @"THiscanbe l";
-        BOOL res = [Solution isStringUnique1:test];
+        NSString *test = @"THisc anbe l";
+        BOOL res = [Solution isStringUnique2:test];
         
     }
     return 0;
