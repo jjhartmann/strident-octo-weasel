a13cbea7dcb0c32351f8bc69a91b9204a725194d
Jeremy J Hartmann <jhartman@sfu.ca>
2016-03-19 00:28:28 -0700

    Add: Solution object skeleton.
    Category extension of NSString.

--- CC1-4-Objc/CC1-4-Objc/main.m
+++ CC1-4-Objc/CC1-4-Objc/main.m
@@ -5,9 +5,23 @@
 //  Created by Jeremy Hartmann on 2016-03-19.
 //  Copyright © 2016 Jeremy Hartmann. All rights reserved.
 //
-//  Replace all ' ' with "%20". 
+//  Replace all ' ' with "%20".
 
 #import <Foundation/Foundation.h>
+
+@interface NSString (EXString)
+- (void)normailzeSpace;
+@end
+
+@implementation NSString (EXString)
+- (void)normailzeSpace
+{
+    
+    
+}
+@end
+
+
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
