d435c4aeb1036e9375ca605c6b342a49e1d6c6d9
Jeremy J Hartmann <jhartman@sfu.ca>
2016-03-18 23:55:55 -0700

    INIT: Add question on comparing strings to identify permutations.

---
+++ CC1-3-Objc/CC1-3-Objc/main.m
@@ -0,0 +1,32 @@
+//
+//  main.m
+//  CC1-3-Objc
+//
+//  Created by Jeremy Hartmann on 2016-03-18.
+//  Copyright © 2016 Jeremy Hartmann. All rights reserved.
+//
+// Check if one string is permutatino of another
+
+
+#import <Foundation/Foundation.h>
+
+@interface EXString : NSString
+- (BOOL)isPermutation:(NSString *)string;
+@end
+
+@implementation EXString
+
+- (BOOL)isPermutation:(NSString *)string
+{
+
+}
+
+@end
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
