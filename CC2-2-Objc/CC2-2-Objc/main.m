230e3c848237b87ec2d4d4d653eefeaa52b9a87b
Jeremy J Hartmann <jhartman@sfu.ca>
2016-03-19 21:39:53 -0700

    INIT: CC2-1 - Kth last element.

---
+++ CC2-2-Objc/CC2-2-Objc/main.m
@@ -0,0 +1,44 @@
+//
+//  main.m
+//  CC2-2-Objc
+//
+//  Created by Jeremy Hartmann on 2016-03-19.
+//  Copyright © 2016 Jeremy Hartmann. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Node : NSObject
+@property Node *next;
+@property NSInteger data;
+
+- (id) initWithData:(NSInteger)data;
+@end
+
+@implementation Node
+
+- (id)initWithData:(NSInteger)data
+{
+    self = [super init];
+    if (self)
+    {
+        _next = nil;
+        _data = 0;
+    }
+    
+    return self;
+}
+@end
+
+
+
+
+
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
