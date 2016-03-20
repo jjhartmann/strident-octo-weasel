8f1d33a56a84a3d6be99d0b50d392f0e59c9bbde
Jeremy J Hartmann <jhartman@sfu.ca>
2016-03-19 22:06:23 -0700

    INIT: Add CC2-3 - Delete node with out previous pointer.

---
+++ CC2-3-Objc/CC2-3-Objc/main.m
@@ -0,0 +1,39 @@
+//
+//  main.m
+//  CC2-3-Objc
+//
+//  Created by Jeremy Hartmann on 2016-03-19.
+//  Copyright © 2016 Jeremy Hartmann. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Node : NSObject
+@property Node *next;
+@property NSInteger data;
+- (id)initWithData:(NSInteger)data;
+@end
+
+@implementation Node
+- (id)initWithData:(NSInteger)data
+{
+    self = [super init];
+    if (self)
+    {
+        _next = nil;
+        _data = data;
+    }
+    
+    return self;
+}
+
+@end
+
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
