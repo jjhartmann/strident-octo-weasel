05a21751f25a45e0005a126e55a40d2c622d86ed
Jeremy J Hartmann <jhartman@sfu.ca>
2016-03-19 23:04:58 -0700

    Add: node class

--- CC2-5/CC2-5/main.m
+++ CC2-5/CC2-5/main.m
@@ -8,6 +8,29 @@
 
 #import <Foundation/Foundation.h>
 
+@interface Node : NSObject
+@property Node *next;
+@property NSInteger data;
+
+- (id)initWithData:(NSInteger)data;
+@end
+
+@implementation Node
+
+- (id)initWithData:(NSInteger)data
+{
+    self = [super init];
+    if (self)
+    {
+        _next = nil;
+        _data = data;
+    }
+    
+    return self;
+}
+
+@end
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
