40948c5ce4565984e898273a90adcbfcfc0b8567
Jeremy J Hartmann <jhartman@sfu.ca>
2016-03-19 18:08:26 -0700

    Add: implementation of is substring. Using a single loop

--- CC1-8-Objc/CC1-8-Objc/main.m
+++ CC1-8-Objc/CC1-8-Objc/main.m
@@ -7,11 +7,56 @@
 //
 
 #import <Foundation/Foundation.h>
+@interface NSString (EXString)
+- (BOOL)isRotation:(NSString *)string;
+@end;
+
+
+@implementation NSString (EXString)
+- (BOOL)isRotation:(NSString *)string
+{
+    NSInteger len1 = self.length;
+    NSInteger len2 = string.length;
+    
+    // If not same lenght, can't be rotation
+    if (len1 != len2)
+    {
+        return NO;
+    }
+    
+    // Iteratre through string until match.
+    int j = 0;
+    BOOL isRotation = NO;
+    for (int i = 0; i < 2*len1; ++i)
+    {
+        if ([self characterAtIndex:i%len1] == [string characterAtIndex:j%len1])
+        {
+            if(!isRotation) isRotation = YES;
+            j++;
+        }
+        else
+        {
+            if (isRotation) isRotation = NO;
+            if (j > 0) j = 0;
+        }
+    }
+    
+    return isRotation;
+}
+@end
+
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        
+        
+        NSString *test =  @"rotations";
+        NSString *test2 = @"ationtrot";
+        
+        BOOL res = [test isRotation:test2];
+        
     }
     return 0;
 }
