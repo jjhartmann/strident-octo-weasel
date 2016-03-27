4a874086bba800e4c1274940850c82df44a1150d
Jeremy J Hartmann <jhartman@sfu.ca>
2016-03-26 20:38:35 -0700

    Add implmenentaion of rod cutting algorithm using DP.

--- CutRodDP-Objc/CutRodDP-Objc/main.m
+++ CutRodDP-Objc/CutRodDP-Objc/main.m
@@ -17,7 +17,24 @@
 
 + (NSInteger)cuttingRod:(NSArray *)price length:(NSInteger)length
 {
+    // Buffer
+    NSInteger buffer[length];
+    bzero(&buffer, sizeof(NSInteger)*length);
+    for (NSInteger i = 0; i < length; ++i)
+    {
+        NSInteger max = INT32_MIN;
+        for (NSInteger j = 0; j <= i; ++j)
+        {
+            NSInteger val = buffer[j] + [[price objectAtIndex:i-j] integerValue];
+            if (max < val)
+                max = val;
+        }
+        buffer[i] = max;
+    }
     
+    
+    
+    return buffer[length - 1];
 }
 
 @end
@@ -26,6 +43,8 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Cutting Rod");
+        NSArray *price = @[@0, @1, @3, @4, @6, @6, @7, @8, @15, @25, @44];
+        NSInteger result = [Solution cuttingRod:price length:9];
     }
     return 0;
 }
