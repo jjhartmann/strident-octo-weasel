13b7258b8c378c5eb89225b108fccfd3d3dac9de
jhartman <jhartman@sfu.ca>
2016-04-01 23:12:59 -0700

    INIT: Question, Multiply Strings.

---
+++ MultiplyStrings/MultiplyStrings/main.cpp
@@ -0,0 +1,15 @@
+//////////////////////////////////////////////////////////////////////
+// Multply Strings
+//       Given two numbers represented as strings, return multiplication of the numbers as a string.
+//       Note: The numbers can be arbitrarily large and are non - negative.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Multiply Strings" << endl;
+
+    return 0;
+}
