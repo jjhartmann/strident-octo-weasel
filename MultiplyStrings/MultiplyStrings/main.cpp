04630a73d0ee7574b57d16eef45af104e2da78ee
jhartman <jhartman@sfu.ca>
2016-04-02 22:49:40 -0700

    Add: Function to add two integer representation of strings.

--- MultiplyStrings/MultiplyStrings/main.cpp
+++ MultiplyStrings/MultiplyStrings/main.cpp
@@ -1,9 +1,13 @@
 //////////////////////////////////////////////////////////////////////
-// Multply Strings
+// Multiply Strings
 //       Given two numbers represented as strings, return multiplication of the numbers as a string.
 //       Note: The numbers can be arbitrarily large and are non - negative.
 
 #include <iostream>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <cstdlib>
 
 using namespace std;
 
@@ -25,8 +29,44 @@ int countInteger(int x)
 
 string addStrings(string a, string b)
 {
+    int alen = a.length();
+    int blen = b.length();
+    int ad = 0; 
+    int bd = 0;
 
-    return "";
+    int maxlen = max(alen, blen);
+
+    if (maxlen != alen){
+        ad = blen - alen;
+    }
+    else {
+        bd = alen - blen;
+    }
+
+    string res = "";
+
+    int carry = 0;
+    for (int i = maxlen - 1; i >= 0; --i)
+    {
+        char ac = ((i - ad) >= 0) ? a[i - ad] : '0';
+        char bc = ((i - bd) >= 0) ? b[i - bd] : '0';
+
+        int ai = atoi(&ac);
+        int bi = atoi(&bc);
+        int sum = ai + bi + carry;
+        
+        int lsb = (sum % 10);
+        carry = static_cast<int>(floor(sum/10)) % 10;
+
+        res = to_string(lsb) + res;
+    }
+
+    if (carry > 0)
+    {
+        res = to_string(carry) + res;
+    }
+
+    return res;
 }
 
 
@@ -35,7 +75,7 @@ int main()
     cout << "Multiply Strings" << endl;
     int res = countInteger(123456789);
     
-
+    string sum = addStrings("923", "88");
 
     return 0;
 }
