d5ac7b67156b50d15ee6a00295284bfce9978109
jhartman <jhartman@sfu.ca>
2016-04-02 22:08:40 -0700

    Add: Function count integer.
    Counts the number of bits in a given integer.

--- MultiplyStrings/MultiplyStrings/main.cpp
+++ MultiplyStrings/MultiplyStrings/main.cpp
@@ -7,9 +7,35 @@
 
 using namespace std;
 
+int countInteger(int x)
+{
+    int sz = sizeof(int) * 8;
+
+    int c = 0;
+    for (int i = 0; i < sz; ++i)
+    {
+        if ((x & (1 << i)) > 0)
+        {
+            c = i;
+        }
+    }
+
+    return c;
+}
+
+string addStrings(string a, string b)
+{
+
+    return "";
+}
+
+
 int main()
 {
     cout << "Multiply Strings" << endl;
+    int res = countInteger(123456789);
+    
+
 
     return 0;
 }
