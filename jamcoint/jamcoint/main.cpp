b092b1612c557bda5d3da0b3f5625c6714fcc25f
jhartman <jhartman@sfu.ca>
2016-04-09 18:31:23 -0700

    Add: TEst stuff>

--- jamcoint/jamcoint/main.cpp
+++ jamcoint/jamcoint/main.cpp
@@ -10,8 +10,73 @@
 
 using namespace std;
 
+int lenofNum(int x)
+{
+    int count = 0;
+    while (x > 0)
+    {
+        x = floor(x / 2);
+        count++;
+    }
+
+    return count;
+}
+
 int main()
 {
+    ofstream ofilet("test.txt");
+    int K = 255;
+    int C = 2;
+    int length = lenofNum(K);
+    for (int i = 0; i <= K; i++)
+    {
+        string org;
+        string gorg;
+        int num = i;
+        for (int j = 0; j < length; ++j)
+        {
+            int val = num % 2;
+            if (val)
+            {
+                org = 'G' + org;
+            }
+            else
+            {
+                org = 'L' + org;
+            }
+
+            gorg += 'G';
+            num = floor(num / 2);
+        }
+        
+        num = i;
+        string res;
+        for (int j = 1; j < C; ++j)
+        {
+            for (int k = 0; k < length; ++k)
+            {
+                int val = num % 2;
+                if (val)
+                {
+                    res = gorg + res;
+                }
+                else
+                {
+                    res = org + res;
+                }
+
+                num = floor(num / 2);
+            }
+        }
+
+        ofilet << res << endl;
+
+    }
+    ofilet.close();
+
+     
+
+
     ifstream file("B-large.in");
     ofstream ofile("sol.txt");
     if (!file.is_open()) return 1;
