2576ae9cb94eae3861d90355ab617f83534e12b3
jhartman <jhartman@sfu.ca>
2016-04-18 17:33:47 -0700

    INIT: Problem, programmer benefit.

---
+++ ProgrammerMaxBenefit/ProgrammerMaxBenefit/main.cpp
@@ -0,0 +1,17 @@
+//////////////////////////////////////////////////////////////////////
+// Programmer Benefit
+//
+// Find the max benefit in assigning k progammers to n jobs. 
+
+
+
+int main()
+{
+
+
+
+
+
+
+    return 0;
+}
