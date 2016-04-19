55d1c2985693489fe7d1d1afb58061f4183cddf2
jhartman <jhartman@sfu.ca>
2016-04-18 17:53:55 -0700

    Add, MaxBenefit function.

--- ProgrammerMaxBenefit/ProgrammerMaxBenefit/main.cpp
+++ ProgrammerMaxBenefit/ProgrammerMaxBenefit/main.cpp
@@ -3,12 +3,30 @@
 //
 // Find the max benefit in assigning k progammers to n jobs. 
 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define BMAP vector<vector<int>>
+
+int MaxBenefit(BMAP b, int k, int N)
+{
+    // 1: build a ratio map with ratio = benefit/programmers
+
+    // 2: Use greedy algorithm to find the max possible values that fulfill k prgrammers 
+
+
+
+
+}
+
 
 
 int main()
 {
 
-
+    
 
 
 
