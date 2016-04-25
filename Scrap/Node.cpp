9df21c49d9e39c92c04a48c2cfe51de0ca0c6cfc
Jeremy J Hartmann <jhartman@sfu.ca>
2016-04-25 16:25:25 -0700

    Scrap/Node.h
    Add: Class Node.
    
    Repersents a single neuron in a network.

---
+++ Scrap/Node.cpp
@@ -0,0 +1,35 @@
+//////////////////////////////////////////////////////////
+// NODE Implementation
+
+#include "Node.h"
+
+
+// Constructor
+Node::Node(float weight)
+{
+		
+}
+
+// Deconstructor
+Node::~Node()
+{
+	
+}
+
+// Mehtod to process data in a feed forward network
+float Node::feedForward(float data)
+{
+	
+	return 0;
+}
+
+// Process error and backpropogate through layers. 
+float Node::backPropogate(float error)
+{
+	
+	
+	return 0;
+}
+
+
+
