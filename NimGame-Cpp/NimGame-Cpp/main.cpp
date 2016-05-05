945aa1472c3d054c3eab87a1c373626d89c4a6e7
jhartman <jhartman@sfu.ca>
2016-05-05 12:32:41 -0400

    Add solution

--- NimGame-Cpp/NimGame-Cpp/main.cpp
+++ NimGame-Cpp/NimGame-Cpp/main.cpp
@@ -8,7 +8,10 @@ using namespace std;
 class Solution {
 public:
     bool canWinNim(int n) {
-
+        // Since both players are playing optimally, 
+        // then any player going first will lose if 
+        // the stack is divisible by 4
+        return !(n % 4 == 0);
     }
 };
 
@@ -17,7 +20,9 @@ int main()
 {
     cout << "Nim Game" << endl;
 
+    Solution sol;
 
+    bool res = sol.canWinNim(4);
 
 
     return 0;
