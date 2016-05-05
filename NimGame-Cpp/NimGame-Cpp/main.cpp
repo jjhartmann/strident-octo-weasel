2eaa675bc38bc65d7b0626720657c4dada06a0c8
jhartman <jhartman@sfu.ca>
2016-05-05 12:19:41 -0400

    INIT: Nim Game

---
+++ NimGame-Cpp/NimGame-Cpp/main.cpp
@@ -0,0 +1,16 @@
+///////////////////////////////////////////////////////////////////////
+// Nim Game Leet Code
+
+#include <iostream>
+
+using namespace std; 
+
+int main()
+{
+    cout << "Nim Game" << endl;
+
+
+
+
+    return 0;
+}
