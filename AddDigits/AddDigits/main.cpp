9227c6b73986b6a31987493183376e0f6e6165f1
jhartman <jhartman@sfu.ca>
2016-05-06 10:39:21 -0400

    Add const time solution.

--- AddDigits/AddDigits/main.cpp
+++ AddDigits/AddDigits/main.cpp
@@ -26,13 +26,37 @@ public:
         return res;
 
     }
+
+    // THe solution can be expressed in constant time. 
+    // Since the solution to the question can only be between 
+    // 0-9, and that the period of is offset every 10th iter
+    // We can then assume that the offset to the sequence equals
+    // the floor of N/9. Which we can use to determine the sequence number
+    // by looking at the last digit.
+    int addDigitsConstantTime(int num) {
+        if (num == 0) return 0;
+
+        int offset = (int)(ceil(num / 9)) % 10;
+
+        if (offset == 0) return 9;
+
+        int n = num % 10;
+
+        if (num % 9 == 0)
+        {
+            offset -= 1;
+        }
+
+        return ((n + offset) % 10);
+
+    }
 };
 
 
 int main()
 {
     Solution sol;
-    int n = sol.addDigits(12356);
+    int n = sol.addDigitsConstantTime(90);
 
 
 
