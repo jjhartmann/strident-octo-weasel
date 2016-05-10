1a5d8f8d7a4f079cb9f5c6f3cc171cf916eb5242
jhartman <jhartman@sfu.ca>
2016-05-10 08:55:45 -0400

    Add solution to finding max depth of binary tree using recursion.

--- MaxDepthofBST-CPP/MaxDepthofBST-CPP/main.cpp
+++ MaxDepthofBST-CPP/MaxDepthofBST-CPP/main.cpp
@@ -20,14 +20,38 @@ struct TreeNode {
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
+        int i = 1;
+        if (root == nullptr) return 0;
 
+        return maxDepthHelper(root, i);
+    }
+
+private:
+    int maxDepthHelper(TreeNode* node, int i)
+    {
+        int l = i;
+        int r = i;
+        
+        ++i;
+        if (node->left != nullptr)
+            l = maxDepthHelper(node->left, i);
+    
+        if (node->right != nullptr)
+            r = maxDepthHelper(node->right, i);
+    
+        return (l < r) ? r : l;
     }
 };
 
 int main()
 {
     cout << "Maximum Depth of Binary Tree" << endl;
+    TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
 
+    Solution sol;
+    sol.maxDepth(root);
 
     return 0;
 }
