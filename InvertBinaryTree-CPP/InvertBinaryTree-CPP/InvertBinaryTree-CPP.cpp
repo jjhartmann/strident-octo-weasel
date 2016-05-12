85160db2a5df50f079ad7a353ec8e3ee8593ea1d
jhartman <jhartman@sfu.ca>
2016-05-12 08:11:25 -0400

    Add recursive method for sol
    switch nodes (sub-trees) in pre-order fashion and recurse.

--- InvertBinaryTree-CPP/InvertBinaryTree-CPP/InvertBinaryTree-CPP.cpp
+++ InvertBinaryTree-CPP/InvertBinaryTree-CPP/InvertBinaryTree-CPP.cpp
@@ -18,7 +18,21 @@ struct TreeNode {
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
+        // Switch pointers and recurse
+        if (root == nullptr) return root;
 
+        TreeNode *p = root->left;
+        
+        root->left = root->right;
+        root->right = p;
+
+        if (root->left != nullptr)
+            invertTree(root->left);
+
+        if (root->right != nullptr)
+            invertTree(root->right);
+
+        return root;
     }
 };
 
