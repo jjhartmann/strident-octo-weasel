d86c9398c0f9a5d25c48d3db7da45ee4ff9499ee
jhartman <jhartman@sfu.ca>
2016-05-12 08:02:15 -0400

    INIT: Invert Binary Tree Problem.

---
+++ InvertBinaryTree-CPP/InvertBinaryTree-CPP/InvertBinaryTree-CPP.cpp
@@ -0,0 +1,18 @@
+// InvertBinaryTree-CPP.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Invert a Binary Tree, inspired by Max Howell" << endl;
+    
+
+
+
+    return 0;
+}
+
