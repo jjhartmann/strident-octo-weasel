dcb2651e4493d2d6b150cb26ac01c52e7383c890
jhartman <jhartman@sfu.ca>
2016-05-12 08:06:35 -0400

    Add solution class and node struct

--- InvertBinaryTree-CPP/InvertBinaryTree-CPP/InvertBinaryTree-CPP.cpp
+++ InvertBinaryTree-CPP/InvertBinaryTree-CPP/InvertBinaryTree-CPP.cpp
@@ -6,6 +6,22 @@
 
 using namespace std;
 
+
+// Definition for a binary tree node.
+struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+
+    }
+};
+
 int main()
 {
     cout << "Invert a Binary Tree, inspired by Max Howell" << endl;
