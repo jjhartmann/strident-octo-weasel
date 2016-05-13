0b56644d7891e29e1b7a17f376ce74921dfdb5f1
jhartman <jhartman@sfu.ca>
2016-05-13 08:31:15 -0400

    INIT: Intersect two arrays problem

---
+++ IntersectionTwoArrays-CPP/IntersectionTwoArrays-CPP/main.cpp
@@ -0,0 +1,24 @@
+/////////////////////////////////////////////////////////////////////
+// Intersection of Two Arrays
+//
+//Given two arrays, write a function to compute their intersection.
+//
+//Example:
+//Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return[2].
+//
+//Note :
+//    Each element in the result must be unique.
+//    The result can be in any order.
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    cout << "INtersection of two arrays." << endl;
+
+    return 0;
+}
