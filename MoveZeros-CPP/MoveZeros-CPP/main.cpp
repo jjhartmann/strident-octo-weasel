d69e038c230da4c627e3214b1e2e9b007d1385c0
jhartman <jhartman@sfu.ca>
2016-05-17 17:22:55 -0400

    Add implementation for inplace solution.

--- MoveZeros-CPP/MoveZeros-CPP/main.cpp
+++ MoveZeros-CPP/MoveZeros-CPP/main.cpp
@@ -9,13 +9,25 @@
 //     Minimize the total number of operations.
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-
+        // Inplace move
+        int size = nums.size();
+        for (int i = 0; i < size; ++i)
+        {
+            if (nums[i] == 0)
+            {
+                nums.erase(nums.begin() + i);
+                nums.push_back(0);
+                --i;
+                --size;
+            }
+        }
     }
 };
 
@@ -23,7 +35,10 @@ public:
 int main()
 {
     cout << "Move Zeros" << endl;
+    vector<int> nums = { 2, 3,4,5,1,0,2,1,0,3,4,5,0,0,3,2,4,0,0 };
+    Solution sol;
 
+    sol.moveZeroes(nums);
 
 
     return 0;
