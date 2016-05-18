f9c1814d535588425f8972d8c81d455ae525f91f
jhartman <jhartman@sfu.ca>
2016-05-18 07:53:35 -0400

    Add Sol Class and Tree def

--- DeleteNodeInLinkedList-CPP/DeleteNodeInLinkedList-CPP/main.cpp
+++ DeleteNodeInLinkedList-CPP/DeleteNodeInLinkedList-CPP/main.cpp
@@ -10,6 +10,21 @@
 using namespace std;
 
 
+
+// Definition for singly-linked list.
+struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+
+    }
+};
+
 int main()
 {
     cout << "Delete Node form Linked List" << endl;
