0b7c80974ea4e238616321312ca6d34a8c7e72fe
jhartman <jhartman@sfu.ca>
2016-05-20 08:30:51 -0400

    Add Solution and Tree Definition

--- SameTree-CPP/SameTree-CPP/main.cpp
+++ SameTree-CPP/SameTree-CPP/main.cpp
@@ -10,6 +10,23 @@
 using namespace std;
 
 
+
+// Definition for a binary tree node.
+struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+
+    }
+};
+
+
 int main()
 {
     cout << "Same Tree" << endl;
