5c5eea1e0a69ded1faacb5b95a87cfd4606f2f59
jhartman <jhartman@sfu.ca>
2016-05-21 11:46:06 -0400

    Add impl method to solve array intersect

--- ArrayIntersection2-CPP/ArrayIntersection2-CPP/main.cpp
+++ ArrayIntersection2-CPP/ArrayIntersection2-CPP/main.cpp
@@ -17,6 +17,8 @@
 
 
 #include <iostream>
+#include <vector>
+#include <unordered_map>
 
 using namespace std;
 
@@ -24,13 +26,50 @@ class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
 
+        // Build hashtable
+        unordered_map<int, int> hmap;
+        for (int i = 0; i < nums1.size(); ++i)
+        {
+            unordered_map<int, int>::iterator got = hmap.find(nums1[i]);
+
+            if (got == hmap.end()) 
+            {
+                hmap.insert(pair<int, int>(nums1[i], 1));
+            }
+            else
+            {
+                got->second++;
+            }
+        }
+
+        // Find intersection
+        vector<int> intersection;
+        for (int i = 0; i < nums2.size(); ++i)
+        {
+            unordered_map<int, int>::iterator got = hmap.find(nums2[i]);
+
+            if (got != hmap.end() && got->second > 0)
+            {
+                intersection.push_back(nums2[i]);
+                got->second--;
+            }
+        }
+
+
+        return intersection;
+
     }
 };
 
 int main()
 {
     cout << "Array Intersection II" << endl;
+    Solution sol;
 
+    vector<int> nums1 = { 1,2,3,4,3,4,5,6,6,7,8,4,9 };
+    vector<int> nums2 = { 2,2,4,5,5,10 ,4};
+
+    vector<int> ret = sol.intersect(nums1, nums2);
 
 
 
