f394479d166252591c0e86a673f838e47208651f
jhartman <jhartman@sfu.ca>
2016-05-23 10:41:31 -0700

    INIT: Valid Anagram Question.

---
+++ VaildAnagram_CPP/VaildAnagram_CPP/main.cpp
@@ -0,0 +1,29 @@
+//////////////////////////////////////////////////////////////////////
+// Vaild Anagram
+//
+// Given two strings s and t, write a function to determine if t is an anagram of s.
+//
+// For example,
+//   s = "anagram", t = "nagaram", return true.
+//   s = "rat", t = "car", return false.
+//
+// Note:
+// You may assume the string contains only lowercase alphabets.
+//
+// Follow up :
+// What if the inputs contain unicode characters ? How would you adapt your solution to such case ?
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Valid Anagram" << endl;
+
+
+
+    return 0;
+}
+
+
