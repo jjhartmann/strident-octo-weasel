f02df82e98374dddd9c7a03db65f898f2391f83b
jhartman <jhartman@sfu.ca>
2016-05-23 11:13:43 -0700

    INIT: Excel Sheet Column Number Question.

---
+++ ExcelSheetColNumber_CPP/ExcelSheetColNumber_CPP/main.cpp
@@ -0,0 +1,31 @@
+//////////////////////////////////////////////////////////////////////
+// Excel Sheet COlumn Number
+//
+// Given a column title as appear in an Excel sheet, return its corresponding column number.
+//
+// For example :
+//
+// A -> 1
+// B -> 2
+// C -> 3
+// ...
+// Z -> 26
+// AA -> 27
+// AB -> 28
+
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    cout << "Excel Sheet Column Number" << endl;
+
+
+
+
+    return 0;
+}
+
