88d06d2fcd46580534ce256b8f1627cd1ff9074f
jhartman <jhartman@sfu.ca>
2016-05-23 11:28:12 -0700

    Add method for solution to Excel column

--- ExcelSheetColNumber_CPP/ExcelSheetColNumber_CPP/main.cpp
+++ ExcelSheetColNumber_CPP/ExcelSheetColNumber_CPP/main.cpp
@@ -21,14 +21,35 @@ using namespace std;
 class Solution {
 public:
     int titleToNumber(string s) {
+        int len = s.length();
+        if (len < 1) return 0;
 
+        // convert to uppercase
+        for (int i = 0; i < len; ++i)
+        {
+            s[i] = toupper(s[i]);
+        }
+
+        int ret = 0;
+        for (int i = 0; i < len - 1; ++i)
+        {
+            ret += pow(26, ((len - 1) - i)) * ((s[i] - 'A') + 1);
+        }
+
+        ret += s[len - 1] - 'A' + 1;
+
+
+        return ret;
     }
+
 };
 
 int main()
 {
     cout << "Excel Sheet Column Number" << endl;
-
+    Solution sol;
+    string str = "AA";
+    sol.titleToNumber("AA");
 
 
 
