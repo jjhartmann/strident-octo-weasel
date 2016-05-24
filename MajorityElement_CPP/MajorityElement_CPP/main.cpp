ea460e6a7bca1999f7bde4afef197ad622ebbf17
jhartman <jhartman@sfu.ca>
2016-05-24 12:40:12 -0700

    Add method for solution to Majority Element.

--- MajorityElement_CPP/MajorityElement_CPP/main.cpp
+++ MajorityElement_CPP/MajorityElement_CPP/main.cpp
@@ -7,19 +7,35 @@
 
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        unordered_map<int, int> hmap;
 
+        int size = nums.size();
+        for (int i = 0; i < size; ++i)
+        {
+            hmap[nums[i]]++;
+
+            if (hmap[nums[i]] > floor(size / 2))
+            {
+                return hmap[nums[i]];
+            }
+        }
+
+        return 0;
     }
 };
 
 int main()
 {
-
+    vector<int> nums = { 2,3,4,3,2,2,2,2 };
+    Solution sol;
+    sol.majorityElement(nums);
 
 
     return 0;
