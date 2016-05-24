09848f077f375b0c74af01ef739c443eec938a11
jhartman <jhartman@sfu.ca>
2016-05-24 13:36:08 -0700

    Add Solution method with hashmap.

--- ContainsDuplicates_CPP/ContainsDuplicates_CPP/main.cpp
+++ ContainsDuplicates_CPP/ContainsDuplicates_CPP/main.cpp
@@ -1,4 +1,4 @@
-//////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
 // Contains Duplicates
 //
 // Given an array of integers, find if the array contains any duplicates.
@@ -7,14 +7,33 @@
 
 #include<iostream>
 #include<vector>
+#include <unordered_set>
 
 using namespace std;
 
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
+        // Can solve either with sorting or hashtable. 
+        unordered_set<int> hmap;
 
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            unordered_set<int>::iterator got = hmap.find(nums[i]);
+            if (got == hmap.end())
+            {
+                hmap.insert(nums[i]);
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
+
+
 };
 
 int main()
