8bdfb961d2974a69c7665b39561fb110f8569a3a
jhartman <jhartman@sfu.ca>
2016-05-24 13:14:22 -0700

    optimized code to use only O(1) space (no data structures).

--- MajorityElement_CPP/MajorityElement_CPP/main.cpp
+++ MajorityElement_CPP/MajorityElement_CPP/main.cpp
@@ -23,11 +23,39 @@ public:
 
             if (hmap[nums[i]] > floor(size / 2))
             {
-                return hmap[nums[i]];
+                return nums[i];
             }
         }
 
         return 0;
+    }
+
+    int majorityElementOpt(vector<int>& nums) {
+        // OPtimization to the majority element problem. 
+        if (nums.size() == 0) return 0;
+
+        int major = nums[0];
+        int c = 1;
+
+        for (int i = 1; i < nums.size(); ++i)
+        {
+            if (nums[i] == major)
+            {
+                c++;
+            }
+            else
+            {
+                c--;
+            }
+
+            if (c <= 0)
+            {
+                c = 1;
+                major = nums[i];
+            }
+        }
+
+        return major;
     }
 };
 
