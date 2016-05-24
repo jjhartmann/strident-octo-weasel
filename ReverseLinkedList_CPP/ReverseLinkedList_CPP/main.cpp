4ada9f7d0e849d8a4ecd5ab371065c59807c176a
jhartman <jhartman@sfu.ca>
2016-05-24 15:10:55 -0700

    INIT: Reverse Linked List Question.

---
+++ ReverseLinkedList_CPP/ReverseLinkedList_CPP/main.cpp
@@ -0,0 +1,15 @@
+///////////////////////////////////////////////////////////////////////
+// Reverse Linked List
+//
+
+#include<iostream>
+
+using namespace std;
+
+
+int main()
+{
+    cout << "reverse Linked List." << endl;
+
+    return 0;
+}
