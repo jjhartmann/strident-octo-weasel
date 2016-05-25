3fcf1635213777af3eaf33410a3acf7478fd59ef
jhartman <jhartman@sfu.ca>
2016-05-25 09:24:55 -0700

    Add solution and node class.

--- ReverseLinkedList_CPP/ReverseLinkedList_CPP/main.cpp
+++ ReverseLinkedList_CPP/ReverseLinkedList_CPP/main.cpp
@@ -7,6 +7,21 @@
 using namespace std;
 
 
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+
+    }
+};
+
 int main()
 {
     cout << "reverse Linked List." << endl;
