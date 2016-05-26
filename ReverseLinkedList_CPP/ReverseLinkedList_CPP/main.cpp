4cea18cca00ec496042ec69887ebfbca811947ca
jhartman <jhartman@sfu.ca>
2016-05-26 12:01:22 -0700

    Add solution to class.

--- ReverseLinkedList_CPP/ReverseLinkedList_CPP/main.cpp
+++ ReverseLinkedList_CPP/ReverseLinkedList_CPP/main.cpp
@@ -19,12 +19,38 @@ class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
 
+        if (head == nullptr) return head;
+        if (head->next == nullptr) return head;
+
+
+        ListNode *p1 = head;
+        ListNode *p2 = head->next;
+        p1->next = nullptr;
+
+        while (p2 != nullptr)
+        {
+            ListNode *tmp = p2->next;
+
+            p2->next = p1;
+            p1 = p2;
+            p2 = tmp;
+        }
+
+        return p1;
     }
 };
+
+
 
 int main()
 {
     cout << "reverse Linked List." << endl;
+    ListNode *head = new ListNode(0);
+    head->next = new ListNode(1);
+    //head->next->next = new ListNode(2);
+
+    Solution sol;
+    sol.reverseList(head);
 
     return 0;
 }
