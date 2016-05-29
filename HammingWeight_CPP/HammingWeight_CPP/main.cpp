1a762068523095346aeaf3df55db3d137c3b526a
jhartman <jhartman@sfu.ca>
2016-05-28 21:55:11 -0700

    INIT: Calculate the Hamming weight of a unsigned int.

---
+++ HammingWeight_CPP/HammingWeight_CPP/main.cpp
@@ -0,0 +1,26 @@
+///////////////////////////////////////////////////////
+// Hamming Weight
+//
+//Write a function that takes an unsigned integer and returns the number of ’1' bits it has (also known as the Hamming weight).
+//
+//For example, the 32 - bit integer ’11' has binary representation 00000000000000000000000000001011, so the function should return 3.
+
+
+#include <iostream>
+
+
+
+using namespace std;
+
+
+
+
+int main()
+{
+    cout << "Calculate the Hamming Weight" << endl;
+
+
+
+
+    return 0;
+}
