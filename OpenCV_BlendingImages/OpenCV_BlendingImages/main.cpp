8d86959b9896a4bf95fecbccd29c6fd85d4812f1
J. J. Hartmann <j3hartma@uwaterloo.ca>
2016-09-22 15:53:45 -0400

    INIT: OpenCV Blending Images

---
+++ OpenCV_BlendingImages/OpenCV_BlendingImages/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenCV_BlendingImages
+//
+//  Created by Jeremy Hartmann on 9/22/16.
+//  Copyright © 2016 Jeremy Hartmann. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
