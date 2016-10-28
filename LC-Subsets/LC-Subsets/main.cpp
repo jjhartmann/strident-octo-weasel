221a929dafccf1748e1684318dd54dc4c40b9b17
Jeremy J Hartmann <jhartman@sfu.ca>
2016-10-28 01:01:43 -0400

    Update starter code

--- LC-Subsets/LC-Subsets/main.cpp
+++ LC-Subsets/LC-Subsets/main.cpp
@@ -7,11 +7,17 @@
 //
 
 #include <iostream>
+#include <vector>
 
+using namespace std;
 
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
+        
+        
+        vector<vector<int>> ret;
+        return ret;
         
     }
 };
