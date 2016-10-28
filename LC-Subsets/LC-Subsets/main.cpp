8b3699965ce0f7d74be44c4b3ee9155f36f39d89
Jeremy J Hartmann <jhartman@sfu.ca>
2016-10-28 01:00:04 -0400

    INIT: LeetCode Question on Subsets

---
+++ LC-Subsets/LC-Subsets/main.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  LC-Subsets
+//
+//  Created by Jeremy Hartmann on 2016-10-28.
+//  Copyright © 2016 Jeremy Hartmann. All rights reserved.
+//
+
+#include <iostream>
+
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    
+    
+    
+    
+    
+    return 0;
+}
