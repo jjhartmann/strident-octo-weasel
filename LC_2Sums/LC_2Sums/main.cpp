aa8f6887b08c042b297c9df85bdda321f36b746a
Jeremy J Hartmann <jhartman@sfu.ca>
2016-10-29 00:57:20 -0400

    INIT: Two Sums

---
+++ LC_2Sums/LC_2Sums/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LC_2Sums
+//
+//  Created by Jeremy Hartmann on 2016-10-29.
+//  Copyright © 2016 Jeremy Hartmann. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
