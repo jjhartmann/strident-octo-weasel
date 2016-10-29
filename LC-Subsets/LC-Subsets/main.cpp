b8ef10077941d6c6213b57cc0367fb3b3e9b87f3
Jeremy J Hartmann <jhartman@sfu.ca>
2016-10-28 23:11:15 -0400

    SOLUTION: For the Subset problem

--- LC-Subsets/LC-Subsets/main.cpp
+++ LC-Subsets/LC-Subsets/main.cpp
@@ -8,17 +8,41 @@
 
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        
-        
         vector<vector<int>> ret;
-        return ret;
         
+        for (int i = 0; i < pow(2, nums.size()); ++i){
+            ret.push_back(createFromBits(i, nums));
+        }
+        
+        return ret;
+    }
+    
+    
+    
+private:
+    vector<int> createFromBits(int x, vector<int> nums) {
+    
+        vector<int> ret;
+        int i = 0;
+        while (x > 0) {
+            
+            if(x % 2) {
+                ret.push_back(nums[i]);
+            }
+            
+            ++i;
+            x = x/2;
+        }
+        
+        
+        return ret;
     }
 };
 
@@ -27,7 +51,9 @@ int main(int argc, const char * argv[]) {
     std::cout << "Hello, World!\n";
     
     
-    
+    vector<int> q = {1,2,3};
+    Solution sol;
+    sol.subsets(q);
     
     
     return 0;
