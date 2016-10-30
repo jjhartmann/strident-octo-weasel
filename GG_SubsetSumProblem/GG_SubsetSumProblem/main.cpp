149606c7daac5e914075691f2fae0c2dcb01c63b
Jeremy J Hartmann <jhartman@sfu.ca>
2016-10-29 22:29:07 -0400

    INIT: Subset Sum Problem

---
+++ GG_SubsetSumProblem/GG_SubsetSumProblem/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GG_SubsetSumProblem
+//
+//  Created by Jeremy Hartmann on 2016-10-29.
+//  Copyright © 2016 Jeremy Hartmann. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
