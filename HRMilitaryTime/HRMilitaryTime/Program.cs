9a76b3e9fde7d79473225e7c694265fb31519842
Jeremy J Hartmann <jhartman@sfu.ca>
2016-12-07 23:16:27 -0500

    Add solution

--- HRMilitaryTime/HRMilitaryTime/Program.cs
+++ HRMilitaryTime/HRMilitaryTime/Program.cs
@@ -13,11 +13,18 @@ namespace HRMilitaryTime
 
 			string[] arr_temp = time.Split(':');
 
+			// Extract AM/PM indicator
+			string indicator = arr_temp[2].Substring(arr_temp[2].Length - 2, 2);
+			arr_temp[2] = arr_temp[2].Substring(0, 2);
 
-		
+			// Convert to int array
+			int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+			if (indicator == "PM")
+			{
+				arr[0] += 12;
+			}
 
-
-
+			Console.WriteLine("{0:D2}:{1:D2}:{2:D2}", arr[0], arr[1], arr[2]);
 
 
 		}
