f9266de810f8bbcbf108bff5ef81bb3bce0b880e
Jeremy J Hartmann <jhartman@sfu.ca>
2016-12-07 23:03:10 -0500

    INIT: Military Time questions

---
+++ HRMilitaryTime/HRMilitaryTime/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HRMilitaryTime
+{
+	class MainClass
+	{
+
+
+
+		public static void Main(string[] args)
+		{
+			string time = Console.ReadLine();
+
+			string[] arr_temp = time.Split(':');
+
+
+		
+
+
+
+
+
+		}
+	}
+}
