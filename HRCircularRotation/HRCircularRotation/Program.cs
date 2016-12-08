cb9c1ce8a230b343c3978a26d49864b115005cb3
Jeremy J Hartmann <jhartman@sfu.ca>
2016-12-08 00:01:10 -0500

    Add sol

--- HRCircularRotation/HRCircularRotation/Program.cs
+++ HRCircularRotation/HRCircularRotation/Program.cs
@@ -10,11 +10,31 @@ namespace HRCircularRotation
 			int n = Convert.ToInt32(tokens_n[0]);
 			int k = Convert.ToInt32(tokens_n[1]);
 			int q = Convert.ToInt32(tokens_n[2]);
+
+			// Edge case
+			if (n == 0)
+			{
+				return;
+			}
+
 			string[] a_temp = Console.ReadLine().Split(' ');
 			int[] a = Array.ConvertAll(a_temp, Int32.Parse);
+
+			int Knorm = k % n;
+
+			if (n == 1)
+			{
+				Console.WriteLine(a[0]);
+				return;
+			}
+
 			for (int a0 = 0; a0 < q; a0++)
 			{
 				int m = Convert.ToInt32(Console.ReadLine());
+
+				// Process the query
+				int index = m - Knorm < 0 ? (n + m) - Knorm : m - Knorm;
+				Console.WriteLine(a[index]);
 			}
 
 
