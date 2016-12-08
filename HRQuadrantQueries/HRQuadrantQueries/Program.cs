7a8e571e8b4f011ec2aa2baba280dcf60dfe84b1
Jeremy J Hartmann <jhartman@sfu.ca>
2016-12-08 00:26:29 -0500

    Add parseing code

--- HRQuadrantQueries/HRQuadrantQueries/Program.cs
+++ HRQuadrantQueries/HRQuadrantQueries/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Collections;
 
 namespace HRQuadrantQueries
 {
@@ -6,7 +9,25 @@ namespace HRQuadrantQueries
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int n = Convert.ToInt32(Console.ReadLine().ToString());
+
+			// Get and store all points
+			List<Tuple<int, int>> points = new List<Tuple<int, int>>();
+			for (int i = 0; i < n; ++i)
+			{
+				string[] arr_t = Console.ReadLine().Split(' ');
+				int[] arr = Array.ConvertAll(arr_t, Int32.Parse);
+
+				points.Add(new Tuple<int, int>(arr[0], arr[1]));
+			}
+
+
+			// Get number of queries and parse
+			int q = Convert.ToInt32(Console.ReadLine().ToString());
+			for (int i = 0; i < q; ++i)
+			{
+				// Process query
+			}
 		}
 	}
 }
