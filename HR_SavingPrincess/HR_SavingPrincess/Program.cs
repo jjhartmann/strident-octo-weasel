5dc5aee9e55d22e32c26931b85a4e3f1255429f8
Jeremy J Hartmann <jhartman@sfu.ca>
2016-12-31 17:51:04 -0400

    Add Soltuion for HR Princess

--- HR_SavingPrincess/HR_SavingPrincess/Program.cs
+++ HR_SavingPrincess/HR_SavingPrincess/Program.cs
@@ -1,12 +1,96 @@
 ﻿using System;
-
-namespace HR_SavingPrincess
+using System.Collections.Generic;
+using System.IO;
+public class Point
 {
-	class MainClass
+	public int mm = 0;
+	public int mn = 0;
+	public Point()
 	{
-		public static void Main(string[] args)
+		; //
+	}
+
+	public Point(int m, int n)
+	{
+		mm = m;
+		mn = n;
+	}
+
+	public Point Diff(Point p)
+	{
+		int dm = mm - p.mm;
+		int dn = mn - p.mn;
+		return new Point(dm, dn);
+	}
+}
+class Solution
+{
+	static void displayPathtoPrincess(int n, String[] grid)
+	{
+		Point pPos = new Point();
+		Point mPos = new Point();
+
+		for (int i = 0; i < n; ++i)
 		{
-			Console.WriteLine("Hello World!");
+			for (int j = 0; j < n; ++j)
+			{
+				// Bot position
+				if (grid[i * n + j] == "m")
+				{
+					mPos.mm = i;
+					mPos.mn = j;
+				}
+
+				// Princess Position
+				if (grid[i * n + j] == "p")
+				{
+					pPos.mm = i;
+					pPos.mn = j;
+				}
+			}
 		}
+
+		// Determine Diff
+		Point diff = mPos.Diff(pPos);
+
+		// Print Navigation for rows.
+		for (int i = diff.mm; i != 0; i = (i < 0) ? i + 1 : i - 1) {
+			if (i < 0)
+			{
+				Console.WriteLine("UP");
+			}
+			else
+			{
+				Console.WriteLine("DOWN");
+			}
+		}
+
+		// Print Navigation for cols.
+		for (int i = diff.mn; i != 0; i = (i < 0) ? i + 1 : i - 1)
+		{
+			if (i < 0)
+			{
+				Console.WriteLine("LEFT");
+			}
+			else
+			{
+				Console.WriteLine("RIGHT");
+			}
+		}
+
+	}
+	static void Main(String[] args)
+	{
+		int m;
+
+		m = int.Parse(Console.ReadLine());
+
+		String[] grid = new String[m];
+		for (int i = 0; i < m; i++)
+		{
+			grid[i] = Console.ReadLine();
+		}
+
+		displayPathtoPrincess(m, grid);
 	}
 }
