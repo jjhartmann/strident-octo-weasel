6f642e169dfc21fb63b5df7a7b9e7d5e4a656054
Jeremy J. Hartmann <jeremy.hartmann@gmail.com>
2017-10-24 18:17:48 -0400

    Update Bit string questions'

--- LC-BitStringEven/LC-BitStringEven/Program.cs
+++ LC-BitStringEven/LC-BitStringEven/Program.cs
@@ -2,11 +2,38 @@
 
 namespace LC_BitStringEven
 {
-    class Program
+    public class Solution
     {
+        public bool HasAlternatingBits(int n)
+        {
+            int prevFlag = n % 2;
+            int altFlag = prevFlag - 1;
+            while (n > 0){
+                if (altFlag == prevFlag){
+                    return false;
+                }
+
+                n = n >> 1;
+                altFlag = prevFlag;
+                prevFlag = n % 2;
+            }
+
+            return true;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Solution sol = new Solution(); 
+            Console.WriteLine(sol.HasAlternatingBits(23452353));
+            Console.WriteLine(sol.HasAlternatingBits(7));
+            Console.WriteLine(sol.HasAlternatingBits(11));
+            Console.WriteLine(sol.HasAlternatingBits(10));
+            Console.WriteLine(sol.HasAlternatingBits(123));
+            Console.WriteLine(sol.HasAlternatingBits(0));
+            Console.WriteLine(sol.HasAlternatingBits(11));
+
         }
     }
+
+
 }
